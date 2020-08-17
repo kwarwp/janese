7dcb73b5e08d4cc737e7259aa6bdc0af54955c8c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 20:57:22 -0300

    Saved grace/main.py @2020-08-17 23:57

--- grace/main.py
+++ grace/main.py
@@ -1,25 +1,26 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
-FUNDO = "https://img.elo7.com.br/product/original/1D27E33/painel-cenario-mundo-encantado-frete-gratis-cenario.jpg"
-LIVRO = "https://comunicamack.files.wordpress.com/2016/12/livro.png"
+FUNDO = "https://i.imgur.com/oGWvqFK.jpg"
+LIVRO = "https://i.imgur.com/vJ7YP8N.png"
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
 
-class inicial():
+class Lago():
 
     def __init__(self):
         self.fundo = Cena(FUNDO)
-        self.livro = Elemento(LIVRO, texto = "Abra este livro!", h=150 , w=150, x=350, y=250)
-        self.mais = Texto(self.fundo, txt = "Teste!", foi = self.mostra_livro) 
+        self.pessoa = Elemento(LIVRO, texto = "O cenário é mesmo bonito!", h=150 , w=150, x=150, y=150)
+        self.mais = Texto(self.fundo, txt = "Alguém está observando este cenário!", foi = self.mostra_pessoa) 
         
-    def mostra_livro(self, *_ ):
-        self.livro.entra(self.fundo)
+    def mostra_pessoa(self, *_ ):
+        self.pessoa.entra(self.fundo)
         
     def chama(self):
         self.fundo.vai()
         self.mais.vai()
-        
-inicial().chama()
+
+if __name__ == "__main__":
+    Lago().chama()
         
