20b17a89523fc41b04be62918b7b2517edf9aa49
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 20:55:48 -0300

    Saved samantha/main.py @2020-08-17 23:55

--- samantha/main.py
+++ samantha/main.py
@@ -2,12 +2,13 @@
 
 from _spy.vitollino.main import Cena, Elemento, STYLE
 
-fundo = "https://media.gazetadopovo.com.br/2019/10/31105652/star-wars-1724901_1920-960x540.jpg"
+FUNDO = "https://media.gazetadopovo.com.br/2019/10/31105652/star-wars-1724901_1920-960x540.jpg"
+LIVRO = ""
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
-class teste():
+class direita():
 
       def _init_(self):
             self.fundo = Cena(FUNDO)
