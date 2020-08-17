fea493bfa0a76ba5dd76c9be4eb06e7a349a6e52
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 20:57:28 -0300

    Saved samantha/main.py @2020-08-17 23:57

--- samantha/main.py
+++ samantha/main.py
@@ -3,7 +3,7 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE
 
 FUNDO = "https://media.gazetadopovo.com.br/2019/10/31105652/star-wars-1724901_1920-960x540.jpg"
-LIVRO = ""
+LIVRO = "https://nerdstore.com.br/wp-content/uploads/2020/02/livro-o-hobbit-edicao-nerdstore-07.jpg"
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
@@ -12,6 +12,7 @@ class direita():
 
       def _init_(self):
             self.fundo = Cena(FUNDO)
+            self.livro = Elemento(LIVRO, texto = "Turma da direita :)", h=150, w=150, x=350, y=250, cena= self.fundo)
             
       def chama(self):
             self.fundo.vai()
