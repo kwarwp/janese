be0ce5c4d8c4b53a78054af167d8f540c84e5764
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 21:03:16 -0300

    Created danae/main.py @2020-08-18 00:03

---
+++ danae/main.py
@@ -0,0 +1,30 @@
+# janese.danae.main.py
+from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+#from janese.danae.main import Lago
+#        self.fundo = Cena(FUNDO, meio=Lago)
+
+
+FUNDO = "https://i.imgur.com/oGWvqFK.jpg"
+LIVRO = "https://i.imgur.com/vJ7YP8N.png"
+
+STYLE["width"] = 900
+STYLE["heigth"] = 900
+
+
+class Lago():
+
+    def __init__(self):
+        self.fundo = Cena(FUNDO)
+        self.pessoa = Elemento(LIVRO, texto = "O cenário é mesmo bonito!", h=150 , w=150, x=150, y=120)
+        self.mais = Texto(self.fundo, txt = "Alguém está observando este cenário!", foi = self.mostra_pessoa) 
+        
+    def mostra_pessoa(self, *_ ):
+        self.pessoa.entra(self.fundo)
+        
+    def vai(self):
+        self.fundo.vai()
+        self.mais.vai()
+
+if __name__ == "__main__":
+    Lago().vai()
+        
