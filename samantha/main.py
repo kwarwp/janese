e3d1ee7a77a0cc8a7df9af512bf519211f72ca1c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 21:14:58 -0300

    Saved samantha/main.py @2020-08-18 00:14

--- samantha/main.py
+++ samantha/main.py
@@ -9,10 +9,11 @@ STYLE["heigth"] = 900
 class turmaDireita():
 
     def ___init__(self):
-            self.fundo = Cena(FUNDO)
-            self.livro = Elemento(LIVRO, texto = "Turma da direita :)", h=150, w=150, x=350, y=250, cena= self.fundo)
+        self.fundo = Cena(FUNDO)
+        self.livro = Elemento(LIVRO, texto = "Turma da direita :)", h=150, w=150, x=350, y=250, cena= self.fundo)
             
     def chama(self):
-            self.fundo.vai()
+        self.fundo.vai()
+
             
 turmaDireita().chama()
