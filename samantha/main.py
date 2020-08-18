20377e685be99ece099e937d5e53cd52870f3b1a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 21:08:30 -0300

    Saved samantha/main.py @2020-08-18 00:08

--- samantha/main.py
+++ samantha/main.py
@@ -3,7 +3,7 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE
 
 FUNDO = "https://media.gazetadopovo.com.br/2019/10/31105652/star-wars-1724901_1920-960x540.jpg"
-LIVRO = "https://nerdstore.com.br/wp-content/uploads/2020/02/livro-o-hobbit-edicao-nerdstore-07.jpg"
+LIVRO = "https://i.pinimg.com/originals/3f/04/04/3f04040d3c0dabe712b06177bc8301b2.jpg"
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
