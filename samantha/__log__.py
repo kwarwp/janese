277f62c50ddc29ddf12218b9f91db36aa6a74870
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 21:00:21 -0300

    Saved samantha/__log__.py @2020-08-18 00:00

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -69,3 +69,21 @@ NameError: name 'inicial' is not defined
     self.fundo.vai()
 AttributeError: 'direita' object has no attribute 'fundo'
 '''},
+{'date': 'Mon Aug 17 2020 21:00:20.282 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 20
+    direita().chama()
+  module <module> line 18
+    self.fundo.vai()
+AttributeError: 'direita' object has no attribute 'fundo'
+'''},
