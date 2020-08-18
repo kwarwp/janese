50a0ceb4d8fae3c270ff8c5fdfc77dfae270a1e2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 21:03:01 -0300

    Saved grace/main.py @2020-08-18 00:03

--- grace/main.py
+++ grace/main.py
@@ -1,29 +1,31 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-#from janese.grace.main import Lago
-#        self.fundo = Cena(FUNDO, meio=Lago)
+from samantha.main import direita 
+from kristen.main import inicialesquerda
+from danae.main import 
 
-
-FUNDO = "https://i.imgur.com/oGWvqFK.jpg"
-LIVRO = "https://i.imgur.com/vJ7YP8N.png"
+FUNDO = "https://img.elo7.com.br/product/original/1D27E33/painel-cenario-mundo-encantado-frete-gratis-cenario.jpg"
+LIVRO = "https://comunicamack.files.wordpress.com/2016/12/livro.png"
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
 
-class Lago():
+class inicial():
 
     def __init__(self):
-        self.fundo = Cena(FUNDO)
-        self.pessoa = Elemento(LIVRO, texto = "O cenário é mesmo bonito!", h=150 , w=150, x=150, y=120)
-        self.mais = Texto(self.fundo, txt = "Alguém está observando este cenário!", foi = self.mostra_pessoa) 
+        """ Criação do fundo, do primeiro elemento (livro) e um texto inicial"""
+        self.fundo = Cena(FUNDO, direita = , esquerda = ) ##
+        self.livro = Elemento(LIVRO, texto = "Abra este livro!", h=150 , w=150, x=350, y=250)
+        self.mais = Texto(self.fundo, txt = "Teste!", foi = self.mostra_livro) 
         
-    def mostra_pessoa(self, *_ ):
-        self.pessoa.entra(self.fundo)
+    def mostra_livro(self, *_ ):
+        """Subordina o aparecimento do livro ao fechamento do texto inicial"""
+        self.livro.entra(self.fundo)
         
-    def vai(self):
+    def chama(self):
+        """ Chama a Cana e o texto inicial"""
         self.fundo.vai()
         self.mais.vai()
-
-if __name__ == "__main__":
-    Lago().vai()
+        
+inicial().chama()
         
