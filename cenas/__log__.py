1f99d36b5a57fcbb9b6c5c15f9335eabf5a0ff71
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 20:01:03 -0300

    Saved cenas/__log__.py @2020-08-24 23:01

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -710,3 +710,19 @@ ImportError: cannot import name 'Inicial'
     self.fundo = Cena(FUNDO, direita =turmaDireita(), esquerda =inicialesquerda(), meio=Lago()) 
 NameError: name 'turmaDireita' is not defined
 '''},
+{'date': 'Mon Aug 24 2020 20:01:01.561 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from cenas.imix import Inicial
+ImportError: cannot import name 'Inicial'
+'''},
