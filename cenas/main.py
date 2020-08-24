933dbb1e948d7739d56ece55c69c4787d516e3f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 16:38:52 -0300

    Saved cenas/main.py @2020-08-24 19:38

--- cenas/main.py
+++ cenas/main.py
@@ -12,8 +12,8 @@ from cenas.imix import Inicial
 
 FUNDO = "https://img.freepik.com/vetores-gratis/fundo-branco-textura-elegante_23-2148415643.jpg?size=626&ext=jpg"
 
-CALEND={IMIX:"https://i.imgur.com/XmmzDHZ.png", 
-            IK:"https://i.imgur.com/9nieFUZ.png",
+CALEND={"IMIX":"https://i.imgur.com/XmmzDHZ.png", 
+            "IK":"https://i.imgur.com/9nieFUZ.png",
            }
 
 STYLE["width"] = 900
