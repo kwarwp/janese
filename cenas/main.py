d62329cf6f159a89d88db0c56dcb07a412cd973c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 14:40:15 -0300

    Created cenas/main.py @2020-08-24 17:40

---
+++ cenas/main.py
@@ -0,0 +1,11 @@
+"""   Tutorial Vitollino para Equipe UNESP 
+
+.. codeauthor:: Equipe Suoygirls <supygirls@gmail.com>
+
+.. version:: 20.01
+     - Adiciona Cena;
+     - Texto inicial 
+     - Texto ao clicar em Elemento;
+     - Import de Salas;
+
+"""
