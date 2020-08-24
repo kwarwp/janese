ccf7188d0b25015b1837fabd15636bd851461bf4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 15:55:14 -0300

    Saved cenas/main.py @2020-08-24 18:55

--- cenas/main.py
+++ cenas/main.py
@@ -7,30 +7,36 @@
 
 """
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-from cenas.imix import nome_a_definir_
-from cenas.ik import nome_a_definir__
+from cenas.imix import Inicial
+#from cenas.ik import nome_a_definir__
 
 FUNDO = "https://img.freepik.com/vetores-gratis/fundo-branco-textura-elegante_23-2148415643.jpg?size=626&ext=jpg"
 IMIX = "https://i.imgur.com/XmmzDHZ.png"
+
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
 
-class estrutura():
+class botao():
+      pass
+         
+
+
+class Estrutura():
 
     def __init__(self):
-        pass
+        self.fundo = Cena(FUNDO)
+        self.imix= Elemento(IMIX, tit="dia 17/08/2020", vai=botao_17082020, x=260, y=170, w=50, h=50, cena =self.fundo)
+         
+    def botao_17082020(self, event = None):
+        Inicial().vai()
         
-    def Botao_17082020(self, *_ ):
+    def botao_24082020(self, *_ ):
         pass
-        
-    def Botao_24082020(self, *_ ):
-        pass
-        
-    def chama_botoes(self):
-        pass
-        
+
+    def partida():
+        self.fundo.vai()
         
 if __name__ == "__main__":        
-    inicial().chama()
+    Estrutura().chama()
