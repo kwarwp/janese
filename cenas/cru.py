38328f6d2ad41ec3dcb5d19bcd9c250f0eb1c347
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 20:48:18 -0300

    Saved cenas/cru.py @2020-08-24 23:48

--- cenas/cru.py
+++ cenas/cru.py
@@ -1,49 +1,20 @@
 # janese.cenas.cru.py
 # SPDX-License-Identifier: GPL-3.0-or-later
-from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-from cenas.imix import Inicial
-from Kristen.main import inicialesquerda
+from _spy.vitollino.main import Cena, Sala, Elemento, STYLE
 
-FUNDO = "https://img.freepik.com/vetores-gratis/fundo-branco-textura-elegante_23-2148415643.jpg?size=626&ext=jpg"
-CALEND={"IMIX":"https://i.imgur.com/XmmzDHZ.png", 
-        "IK":"https://i.imgur.com/9nieFUZ.png",
-        "AKBAL":"XX",
-        "KAN":"XX",
-        "CHICCHAN":"XX",
-        "CIMI":"XX",
-        "MANIK":"XX",
-        "LAMAT":"XX",
-        "MALUC":"XX",
-        "OC":"XX",
-        "CHUEN":"XX",
-        "EB":"XX",
-        "AKBAL":"XX"
-       }
        
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
 
-class Estrutura():
+class Passeio():
 
 
     def __init__(self):
     
-        self.fundo=Cena(FUNDO)
-        self.fundo.vai()
-        """ Primeiro modo de criar um botão. Utiliza a função vai() do vitollino"""
-        self.imix=Elemento(CALEND["IMIX"], tit="dia 17/08/2020", x=0, y=100, w=300, h=300, cena=self.fundo, vai=self.botao_17082020)
-
-        """Segundo modo de criar um botão. """
-        self.ik= Elemento(CALEND["IK"], tit="dia 24/08/2020", x=300, y=100, w=300, h=300, cena=self.fundo)
-        self.ik.elt.bind("click", self.botao_24082020)
+      
         
-    def botao_17082020(self, event=None):
-        """ Método que permite a instância do botão."""
-        Inicial().chama()
-    def botao_24082020(self, event=None):
-        #inicialesquerda()
-        Inicial().chama()
+
             
 if __name__ == "__main__":
-    Estrutura()
+    Passeio()
