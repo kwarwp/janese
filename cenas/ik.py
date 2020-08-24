4e7cab078778f6158a95695dc75e4f40ff327c69
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 17:58:43 -0300

    Saved cenas/ik.py @2020-08-24 20:58

--- cenas/ik.py
+++ cenas/ik.py
@@ -13,9 +13,27 @@ Changelog
 """
 from _spy.vitollino.main import Labirinto, Musica
 
+SALA="https://p2.trrsf.com/image/fget/cf/460/0/images.terra.com/2018/03/23/modelos-de-escadas-linear-sem-corrimao.jpg"
+ESCRITORIO="https://www.viveremcasa.com/wp-content/uploads/2018/02/cadeira-home-office-preta-couro-comprar.jpg"
+CORREDOR"https://i.pinimg.com/originals/b0/de/8a/b0de8ab7aa396b97d7f43d8289727146.jpg"
+QUARTO="https://i.pinimg.com/originals/61/af/f4/61aff4dcbf8fdfe0ec7faa77bf310f64.jpg"
+BANHEIRO="https://www.iquilibrio.com/blog/wp-content/uploads/2017/03/71245-feng-shui-no-banheiro-veja-como-aplicar-em-sua-casa-1.jpg"
+
 
 class Passeio():
-    pass
+
+    def __init__(self):
+        entrada=Labirinto(
+        entrada.vai()
+    
+    def cozinha(self):
+        pass
+        
+    def sala(self):
+        pass
+        
+    def floor_two(self):
+        pass
     
     
 if __name__ = "__main__":
