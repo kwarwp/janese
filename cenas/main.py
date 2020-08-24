00986e672227295ae97a3d73c71764115d07c348
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 14:55:16 -0300

    Saved cenas/main.py @2020-08-24 17:55

--- cenas/main.py
+++ cenas/main.py
@@ -1,42 +1,33 @@
-"""   Tutorial Vitollino para Equipe UNESP 
+"""   Vitollino para Equipe UNESP 
 
 .. codeauthor:: Equipe Suoygirls <supygirls@gmail.com>
 
 .. version:: 20.01
-     - Adiciona Cena;
-     - Texto inicial 
-     - Texto ao clicar em Elemento;
-     - Import de Salas;
+     - Botões para cada 
 
 """
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-from samantha.main import turmaDireita
-from kristen.main import inicialesquerda
-from danae.main import Lago
+from cenas.imix import nome_a_definir_
+from cenas.ik import nome_a_definir__
 
-FUNDO = "https://img.elo7.com.br/product/original/1D27E33/painel-cenario-mundo-encantado-frete-gratis-cenario.jpg"
-LIVRO = "https://comunicamack.files.wordpress.com/2016/12/livro.png"
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
 
-class inicial():
+class estrutura():
 
     def __init__(self):
-        """ Criação do fundo. Importação de módulos , do primeiro elemento (livro) e um texto inicial"""
-        self.fundo = Cena(FUNDO, direita =turmaDireita(), esquerda =inicialesquerda(), meio=Lago()) 
-        self.livro = Elemento(LIVRO, texto = "Abra este livro!", h=150 , w=150, x=350, y=250)
-        self.mais = Texto(self.fundo, txt = "Teste!", foi = self.mostra_livro) 
+        pass
         
-    def mostra_livro(self, *_ ):
-        """Subordina o aparecimento do livro ao fechamento do texto inicial"""
-        self.livro.entra(self.fundo)
+    def Botao_17082020(self, *_ ):
+        pass
         
-    def chama(self):
-        """ Chama a Cana e o texto inicial"""
-        self.fundo.vai()
-        self.mais.vai()
+    def Botao_24082020(self, *_ ):
+        pass
+        
+    def chama_botoes(self):
+        pass
         
         
 if __name__ == "__main__":        
