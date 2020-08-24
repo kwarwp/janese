58a36cdf7e3361a0d4adb754e18487de7c33dbf7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 15:30:22 -0300

    Saved grace/main.py @2020-08-24 18:30

--- grace/main.py
+++ grace/main.py
@@ -1,32 +1,3 @@
-from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-from samantha.main import turmaDireita
-from kristen.main import inicialesquerda
-from danae.main import Lago
-
-FUNDO = "https://img.elo7.com.br/product/original/1D27E33/painel-cenario-mundo-encantado-frete-gratis-cenario.jpg"
-LIVRO = "https://comunicamack.files.wordpress.com/2016/12/livro.png"
-
-STYLE["width"] = 900
-STYLE["heigth"] = 900
-
-
-class inicial():
-
-    def __init__(self):
-        """ Criação do fundo. Importação de módulos , do primeiro elemento (livro) e um texto inicial"""
-        self.fundo = Cena(FUNDO, direita =turmaDireita(), esquerda =inicialesquerda(), meio=Lago()) 
-        self.livro = Elemento(LIVRO, texto = "Abra este livro!", h=150 , w=150, x=350, y=250)
-        self.mais = Texto(self.fundo, txt = "Teste!", foi = self.mostra_livro) 
-        
-    def mostra_livro(self, *_ ):
-        """Subordina o aparecimento do livro ao fechamento do texto inicial"""
-        self.livro.entra(self.fundo)
-        
-    def chama(self):
-        """ Chama a Cana e o texto inicial"""
-        self.fundo.vai()
-        self.mais.vai()
-        
-        
-if __name__ == "__main__":        
-    inicial().chama()
+# janese.grace.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+#from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
