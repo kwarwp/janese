45b24b01a2e1e40fa7d982de63dd0edce1f73764
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 20:07:24 -0300

    Saved cenas/__log__.py @2020-08-24 23:07

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -823,3 +823,21 @@ IndentationError: unexpected indent
     self.imix=Elemento(CALEND["IMIX"], tit="dia 17/08/2020", x=0, y=100, w=300, h=300, cena=self.fundo, vai=botao_17082020)
 NameError: name 'botao_17082020' is not defined
 '''},
+{'date': 'Mon Aug 24 2020 20:07:22.256 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    Estrutura()
+  module <module> line 36
+    self.imix=Elemento(CALEND["IMIX"], tit="dia 17/08/2020", x=0, y=100, w=300, h=300, cena=self.fundo, vai=botao_17082020)
+NameError: name 'botao_17082020' is not defined
+'''},
