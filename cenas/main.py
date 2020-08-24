1d0e31c1574c9234f1abfd4b1e07728bcdf39d96
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 16:38:08 -0300

    Saved cenas/main.py @2020-08-24 19:38

--- cenas/main.py
+++ cenas/main.py
@@ -11,8 +11,10 @@ from cenas.imix import Inicial
 #from cenas.ik import nome_a_definir__
 
 FUNDO = "https://img.freepik.com/vetores-gratis/fundo-branco-textura-elegante_23-2148415643.jpg?size=626&ext=jpg"
-IMIX = "https://i.imgur.com/XmmzDHZ.png"
 
+CALEND={IMIX:"https://i.imgur.com/XmmzDHZ.png", 
+            IK:"https://i.imgur.com/9nieFUZ.png",
+           }
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
@@ -27,9 +29,11 @@ class Estrutura():
         self.imix= Elemento(IMIX, tit="dia 17/08/2020", x=0, y=0, w=300, h=300, cena =self.fundo, vai=self.botao_17082020)
          
     def botao_17082020(self, event=None):
+        """ Método que será chamado no clique do IMIX"""
         Inicial().chama()
         
     def botao_24082020(self, *_ ):
+        """ Método que será chamado no clique do IK"""
         pass
 
         
