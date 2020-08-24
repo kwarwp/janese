30c56d432c1bf12011d1018750c484e35e8253b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 15:08:09 -0300

    Created cenas/__log__.py @2020-08-24 18:08

---
+++ cenas/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 24 2020 15:08:06.568 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 37
+  self.livro = Elemento(LIVRO, tit = "LIVRO" texto = "Era uma vez...", h=300, w=300, x=350, y=250)
+                                              ^
+SyntaxError: invalid syntax
+'''},
