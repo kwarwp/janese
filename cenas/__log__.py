8473a10e00944f955349132f8cdbc1483ee90afe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 19:08:59 -0300

    Saved cenas/__log__.py @2020-08-24 22:08

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -540,3 +540,21 @@ AttributeError: 'Passeio' object has no attribute 'escrit'
     self.escrit = Cena(SALA, meio=self.escrit, direita=self.partida,  cena =self.partida)
 NameError: name 'Cena' is not defined
 '''},
+{'date': 'Mon Aug 24 2020 19:08:57.171 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    Passeio().vai()
+  module <module> line 27
+    self.escrit.n.vai()
+AttributeError: 'Cena' object has no attribute 'n'
+'''},
