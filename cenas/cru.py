41233755ac3b92a49c24b90dba398708f6009a5f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 20:05:49 -0300

    Saved cenas/cru.py @2020-08-24 23:05

--- cenas/cru.py
+++ cenas/cru.py
@@ -25,13 +25,13 @@ STYLE["heigth"] = 900
 
 class Estrutura():
 
-     def __init__(self):
+    def __init__(self):
     
         self.fundo=Cena(FUNDO)
         self.fundo.vai()
         
         self.imix=Elemento(CALEND["IMIX"], tit="dia 17/08/2020", x=0, y=100, w=300, h=300, cena=self.fundo, vai=botao_17082020)
-        
+    
     def botao_17082020(self, event=None):
         Inicial().chama()
             
