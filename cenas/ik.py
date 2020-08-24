c62762c08af92c7c4569f2116d49c28512e617bf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 17:21:01 -0300

    Created cenas/ik.py @2020-08-24 20:21

---
+++ cenas/ik.py
@@ -0,0 +1,15 @@
+# janese.cenas.ik.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Equipe Supygirls <supygirls@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.08
+        - Implementação do Labirinto
+        - Implementação música
+
+"""
+from _spy.vitollino.main import Labirinto, Musica
+
