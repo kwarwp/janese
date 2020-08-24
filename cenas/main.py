4b60ce90b30302385a718dd6bcec1113ce662232
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 16:22:28 -0300

    Saved cenas/main.py @2020-08-24 19:22

--- cenas/main.py
+++ cenas/main.py
@@ -24,10 +24,10 @@ class Estrutura():
         self.fundo = Cena(FUNDO)
         self.fundo.vai()
         
-        self.imix= Elemento(IMIX, tit="dia 17/08/2020", x=0, y=0, w=300, h=300, cena =self.fundo, vai=botao_17082020())
+        self.imix= Elemento(IMIX, tit="dia 17/08/2020", x=0, y=0, w=300, h=300, cena =self.fundo, vai=self.botao_17082020)
          
-    def botao_17082020(self, event = None):
-        return Inicial()
+    def botao_17082020(self):
+        Inicial().chama()
         
     def botao_24082020(self, *_ ):
         pass
