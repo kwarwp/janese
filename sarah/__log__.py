c39419f1a799ce59ac183d66864f3d662785691d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 19:50:06 -0300

    Created sarah/__log__.py @2020-08-24 22:50

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 24 2020 19:50:04.116 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  CALEND= 
+           ^
+SyntaxError: invalid syntax
+'''},
