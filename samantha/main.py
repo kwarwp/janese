8511e42e55d9ed2c7e444927b2d100cbf9e0c132
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 19:59:29 -0300

    Saved samantha/main.py @2020-08-24 22:59

--- samantha/main.py
+++ samantha/main.py
@@ -1,19 +1,37 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE, texto
 
-FUNDO = "https://media.gazetadopovo.com.br/2019/10/31105652/star-wars-1724901_1920-960x540.jpg"
-LIVRO = "https://i.pinimg.com/originals/3f/04/04/3f04040d3c0dabe712b06177bc8301b2.jpg"
+FUNDO = "https://images-americanas.b2w.io/produtos/01/00/img4/184202/8/184202888_1GG.jpg"
+
+
+CALEND={"IMIX":"https://i.imgur.com/XmmzDHZ.png", 
+        "IK":"https://i.imgur.com/9nieFUZ.png",
+        "AKBAL":"XX",
+        "KAN":"XX",
+        "CHICCHAN":"XX",
+        "CIMI":"XX",
+        "MANIK":"XX",
+        "LAMAT":"XX",
+        "MALUC":"XX",
+        "OC":"XX",
+        "CHUEN":"XX",
+        "EB":"XX",
+        "AKBAL":"XX"
+       }
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
-class samanthaNog():
+class turmadaDireita():
 
-    def __init__(self):
-        self.fundo = Cena(FUNDO)
-        self.livro = Elemento(LIVRO, texto = "Turma da direita :)", h=150, w=150, x=350, y=250, cena= self.fundo)
-            
-    def vai(self):
+     def __init__(self):
+    
+        self.fundo=Cena(FUNDO)
         self.fundo.vai()
-
-if __name__ == "__main__":     
-    turmaDireita().vai()
+        
+        self.imix=Elemento(CALEND["IMIX"], tit="dia 17/08/2020", x=0, y=100, w=100, h=100, cena=self.fundo, vai=botao_17082020)
+        
+        def botao_17082020(self, event=None):
+            Inicial().chama()
+            
+if __name__ == "__main__":
+    turmadaDireita()
