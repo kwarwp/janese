129cfe0c1efddd00bea1ef7e9a507cdd339b7526
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 18:18:25 -0300

    Saved cenas/__log__.py @2020-08-24 21:18

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -314,3 +314,21 @@ AttributeError: 'Passeio' object has no attribute 'cozinha'
     self.partida.vai()
 AttributeError: 'Sala' object has no attribute 'vai'
 '''},
+{'date': 'Mon Aug 24 2020 18:18:23.649 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    Passeio()
+  module <module> line 27
+    self.partida.vai()
+AttributeError: 'Sala' object has no attribute 'vai'
+'''},
