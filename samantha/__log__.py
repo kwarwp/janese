0d279942b8a19bced2db527b48823f13053243c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 20:01:01 -0300

    Saved samantha/__log__.py @2020-08-24 23:01

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -328,3 +328,34 @@ AttributeError: 'turmaDireita' object has no attribute 'fundo'
     self.fundo.vai()
 AttributeError: 'turmaDireita' object has no attribute 'fundo'
 '''},
+{'date': 'Mon Aug 24 2020 20:00:59.31 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 1
+    from _spy.vitollino.main import Cena, Elemento, STYLE, texto
+ImportError: cannot import name 'texto'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 1
+    from _spy.vitollino.main import Cena, Elemento, STYLE, texto
+'''},
