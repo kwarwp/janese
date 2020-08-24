f40bc147b725a35cd7870961ff4da714711f9f52
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 19:58:36 -0300

    Created lilianesquinelato/__log__.py @2020-08-24 22:58

---
+++ lilianesquinelato/__log__.py
@@ -0,0 +1,40 @@
+
+{'date': 'Mon Aug 24 2020 19:58:30.761 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    from cenas.imix import Inicial
+  module cenas.imix line 18
+    from samantha.main import turmaDireita
+  module samantha.main line 1
+    from _spy.vitollino.main import Cena, Elemento, STYLE, texto
+ImportError: cannot import name 'texto'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    from cenas.imix import Inicial
+  module cenas.imix line 18
+    from samantha.main import turmaDireita
+  module samantha.main line 1
+    from _spy.vitollino.main import Cena, Elemento, STYLE, texto
+'''},
