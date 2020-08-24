343ea2f6849f79cbf14552a0c76c23923a0b8f26
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 19:13:48 -0300

    Saved cenas/__log__.py @2020-08-24 22:13

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -565,3 +565,21 @@ AttributeError: 'Cena' object has no attribute 'n'
                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 24 2020 19:13:46.479 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    Passeio().vai()
+  module <module> line 25
+    self.colecao = Sala(n=self.escrit, l= self.cozinha, o= self.cozinha)
+AttributeError: 'Passeio' object has no attribute 'escrit'
+'''},
