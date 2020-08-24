bd6015fdaab0c8551c0a31a8d9502895d779d4a0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 20:03:06 -0300

    Saved cenas/__log__.py @2020-08-24 23:03

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -780,3 +780,21 @@ UnboundLocalError: local variable 'botao_17082020' referenced before assignment
     self.imix=Elemento(CALEND["IMIX"], tit="dia 17/08/2020", x=0, y=100, w=100, h=100, cena=self.fundo, vai=botao_17082020)
 UnboundLocalError: local variable 'botao_17082020' referenced before assignment
 '''},
+{'date': 'Mon Aug 24 2020 20:03:04.472 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    Estrutura()
+  module <module> line 33
+    self.imix=Elemento(CALEND["IMIX"], tit="dia 17/08/2020", x=0, y=100, w=300, h=300, cena=self.fundo, vai=botao_17082020)
+UnboundLocalError: local variable 'botao_17082020' referenced before assignment
+'''},
