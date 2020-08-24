d8cc701b07e4809aa12901b7d16f00947c26d566
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 15:52:06 -0300

    Saved cenas/__log__.py @2020-08-24 18:52

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -6,3 +6,19 @@
                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 24 2020 15:52:04.10 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    inicial().chama()
+NameError: name 'inicial' is not defined
+'''},
