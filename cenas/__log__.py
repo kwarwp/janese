4ca4586e47eb98d40d35e0c58ea9b2a973ca56f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 18:17:11 -0300

    Saved cenas/__log__.py @2020-08-24 21:17

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -278,3 +278,21 @@ SyntaxError: invalid syntax
                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 24 2020 18:17:08.980 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 43
+    Passeio()
+  module <module> line 26
+    self.partida=Sala(n=self.sala , l=self.cozinha)
+AttributeError: 'Passeio' object has no attribute 'cozinha'
+'''},
