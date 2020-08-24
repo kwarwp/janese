cf6caaa3a3576fc816ace6b7174e60c7c6370bce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 19:55:01 -0300

    Created carlo/__log__.py @2020-08-24 22:55

---
+++ carlo/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 24 2020 19:54:59.502 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 40
+  def vai(self):
+  ^
+IndentationError: unexpected indent
+'''},
