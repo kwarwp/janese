31a816e271ae128b97b731908adca63c098e085a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-24 15:55:25 -0300

    Saved cenas/__log__.py @2020-08-24 18:55

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -22,3 +22,21 @@ SyntaxError: invalid syntax
     inicial().chama()
 NameError: name 'inicial' is not defined
 '''},
+{'date': 'Mon Aug 24 2020 15:55:23.39 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    Estrutura().chama()
+  module <module> line 30
+    self.imix= Elemento(IMIX, tit="dia 17/08/2020", vai=botao_17082020, x=260, y=170, w=50, h=50, cena =self.fundo)
+NameError: name 'botao_17082020' is not defined
+'''},
