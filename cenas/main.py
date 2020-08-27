16773b6d832a4e4864750fb263149460ab02f840
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-26 22:40:38 -0300

    Saved cenas/main.py @2020-08-27 01:40

--- cenas/main.py
+++ cenas/main.py
@@ -7,8 +7,9 @@
 
 """
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-from cenas.imix import Passeio
-
+from cenas.imix import Inicial
+#from cenas.ik import Passeio
+from cena.ik import Passeio
 
 FUNDO = "https://img.freepik.com/vetores-gratis/fundo-branco-textura-elegante_23-2148415643.jpg?size=626&ext=jpg"
 
