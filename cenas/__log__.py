ba3c540a1004fe06bdab6bb6b9c9821be8e0513d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-26 22:47:41 -0300

    Saved cenas/__log__.py @2020-08-27 01:47

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1051,3 +1051,21 @@ AttributeError: 'Passeio' object has no attribute 'escrit'
     self.colecao = Sala(n=comodo_sala, l=comodo_cozinha)
 NameError: name 'comodo_sala' is not defined
 '''},
+{'date': 'Wed Aug 26 2020 22:47:40.280 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 43
+    Passeio()
+  module <module> line 34
+    self.colecao.norte.vai()
+AttributeError: 'method' object has no attribute 'vai'
+'''},
