684d9270a2b35d9c492689fe9fa5dc120354cc4f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-26 22:30:21 -0300

    Saved cenas/ik.py @2020-08-27 01:30

--- cenas/ik.py
+++ cenas/ik.py
@@ -7,7 +7,9 @@
 Changelog
 ---------
 .. versionadded::    20.08
-      -
+      - Criação de múltipla cenas
+      - Concatenação das múltiplas cenas (Sala)
+      - Uso de botões para acessar segundo andar
 
 """
 from _spy.vitollino.main import Sala, Cena, STYLE
@@ -19,14 +21,32 @@ QUARTO="https://i.pinimg.com/originals/61/af/f4/61aff4dcbf8fdfe0ec7faa77bf310f64
 BANHEIRO="https://www.iquilibrio.com/blog/wp-content/uploads/2017/03/71245-feng-shui-no-banheiro-veja-como-aplicar-em-sua-casa-1.jpg"
 COZINHA="https://i.pinimg.com/originals/18/bf/1b/18bf1b40b41a7d86b5913c38f6492799.jpg"
 
+INVISI = "https://i.imgur.com/JuvyDuW.png"
+
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
+class botao():
+
+    def __init__(self,Elemento, acao, caminho, event=None):
+        self.Elemento = Elemento
+        self.acao = clica
+        self.caminho = caminho
+        
+    def Elemento():
+        from _spy.vitollino.main import Elemento
+        
+    def clica(self):
+        return Elemento.elt.bind("click", caminho)
+    
+    
 class Passeio():
 
     def __init__(self):
         self.escrit = Cena(SALA)
-        self.cozinha = Cena(COZINHA)
+        self.cozinha = Cena(COZINHA, direita = self.escrit)
+        
+        self.botaoteste = (
         
         self.colecao = Sala(n=self.escrit, l= self.cozinha, o= self.cozinha)
         self.colecao.norte.vai()
