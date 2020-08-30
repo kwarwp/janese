47e8b09254a6caba4806e6d4a06b2fdc25bf54f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-30 19:47:39 -0300

    Saved cenas/ik.py @2020-08-30 22:47

--- cenas/ik.py
+++ cenas/ik.py
@@ -12,17 +12,17 @@ Changelog
       - Uso de botões para acessar segundo andar
 
 """
-from _spy.vitollino.main import Sala, Cena, STYLE
+from _spy.vitollino.main import Sala, Cena, Labirinto,STYLE
 
-SALA_UM={NORTE="https://i.imgur.com/hqK4K6C.png",
-         LESTE="https://i.imgur.com/IY0ZWKp.png",
-         SUL="https://i.imgur.com/YDEzStv.png",
-         OESTE="https://i.imgur.com/8HC6cMG.png"}
+SALA_UM={"NORTE": "https://i.imgur.com/hqK4K6C.png",
+         "LESTE":"https://i.imgur.com/IY0ZWKp.png",
+         "SUL":"https://i.imgur.com/YDEzStv.png",
+         "OESTE":"https://i.imgur.com/8HC6cMG.png"}
          
-SALA_DOIS={NORTE="https://i.imgur.com/sH2GD9s.png",
-           LESTE="https://i.imgur.com/R7JE7pM.png",
-           SUL="https://i.imgur.com/BSov4HG.png",
-           OESTE="https://i.imgur.com/yBuIbjQ.png"}
+SALA_DOIS={"NORTE":"https://i.imgur.com/sH2GD9s.png",
+           "LESTE":"https://i.imgur.com/R7JE7pM.png",
+           "SUL":"https://i.imgur.com/BSov4HG.png",
+           "OESTE":"https://i.imgur.com/yBuIbjQ.png"}
 
 
 INVISI = "https://i.imgur.com/JuvyDuW.png"
@@ -32,16 +32,30 @@ STYLE["heigth"] = 900
 
         
 class Passeio():
-
-    def __init__(self):
-        self.colecao = Sala(n=self.comodo_sala, l=self.comodo_cozinha)
-        self.colecao.norte.vai()
-        
-    def comodo_cozinha(self):
-        return self.cozinha = Cena(COZINHA, esquerda = self.sala)
-        
-    def comodo_sala(self):
-        return self.sala = Cena(SALA, direita = self.cozinha)
+    """Gera um labirinto com duas salas
+    """
+    def __init__(self,vai):
+        """Start com coleção de Salas"""
+        self.colacao_total=Labirinto(n=self.sala_um,c=self.sala_dois)
+        self.colacao_total.norte.vai()
+    
+    def sala_um(self):
+        """Coleção de cenas 1"""
+        self.norte_um = Cena(SALA_UM["NORTE"])
+        self.leste_um = Cena(SALA_UM["LESTE"])
+        self.oeste_um = Cena(SALA_UM["OESTE"])
+        self.sul_um = Cena(SALA_UM["SUL"])
+        self.colecao_um = Sala(n=self.norte_um,l=self.leste_um,s=self.sul_um,o=self.oeste_um) 
+        return self.colecao_um.norte.vai()
+                
+    def sala_dois(self):
+        """Coleção de cenas 2"""
+        self.norte_dois = Cena(SALA_UM["NORTE"])
+        self.leste_dois = Cena(SALA_UM["LESTE"])
+        self.oeste_dois = Cena(SALA_UM["OESTE"])
+        self.sul_dois = Cena(SALA_UM["SUL"])
+        self.colecao_dois = Sala(n=self.norte_dois,l=self.leste_dois,s=self.sul_dois,o=self.oeste_dois)
+        return self.colecao_dois.norte.vai() 
     
 if __name__ == "__main__":
     Passeio()
