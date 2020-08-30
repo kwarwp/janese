e6ce770e0395d36381ab934ad8655806607e5bf2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-30 18:24:15 -0300

    Saved cenas/ik.py @2020-08-30 21:24

--- cenas/ik.py
+++ cenas/ik.py
@@ -14,12 +14,11 @@ Changelog
 """
 from _spy.vitollino.main import Sala, Cena, STYLE
 
-SALA="https://p2.trrsf.com/image/fget/cf/460/0/images.terra.com/2018/03/23/modelos-de-escadas-linear-sem-corrimao.jpg"
-ESCRITORIO="https://www.viveremcasa.com/wp-content/uploads/2018/02/cadeira-home-office-preta-couro-comprar.jpg"
-CORREDOR="https://i.pinimg.com/originals/b0/de/8a/b0de8ab7aa396b97d7f43d8289727146.jpg"
-QUARTO="https://i.pinimg.com/originals/61/af/f4/61aff4dcbf8fdfe0ec7faa77bf310f64.jpg"
-BANHEIRO="https://www.iquilibrio.com/blog/wp-content/uploads/2017/03/71245-feng-shui-no-banheiro-veja-como-aplicar-em-sua-casa-1.jpg"
-COZINHA="https://i.pinimg.com/originals/18/bf/1b/18bf1b40b41a7d86b5913c38f6492799.jpg"
+SALA_UM={NORTE="https://i.imgur.com/hqK4K6C.png",
+         LESTE="https://i.imgur.com/IY0ZWKp.png",
+         SUL="https://i.imgur.com/YDEzStv.png",
+         OESTE="https://i.imgur.com/8HC6cMG.png"}
+
 
 INVISI = "https://i.imgur.com/JuvyDuW.png"
 
