b1f90f1ddfa380d565d3a7815cd6ca6ab4fc90d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-31 17:56:47 -0300

    Saved cenas/__log__.py @2020-08-31 20:56

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1703,3 +1703,19 @@ AttributeError: 'ambiente' object has no attribute 'maca'
     return inv.bota(self.maca)
 AttributeError: 'ambiente' object has no attribute 'maca'
 '''},
+{'date': 'Mon Aug 31 2020 17:56:46.111 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    ambiente()
+NameError: name 'ambiente' is not defined
+'''},
