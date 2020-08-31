0b025af4f7d612d83b7ae7cef32d232472d5287d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-31 18:28:50 -0300

    Saved cenas/__log__.py @2020-08-31 21:28

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1781,3 +1781,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Aug 31 2020 18:28:50.274 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  if __name__ == "__main__":
+  ^
+IndentationError: expected an indented block
+'''},
