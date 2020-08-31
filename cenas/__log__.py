899afb81035239f093580874c0ec5d50337ddfdf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-31 20:29:05 -0300

    Saved cenas/__log__.py @2020-08-31 23:29

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1869,3 +1869,21 @@ SyntaxError: invalid syntax
     self.laranja=Elemento(ITEM["LARANJA"], tit="laranja",style=dict(height=60,widht=60, left=100, top=100),drag=True,cena=self.fundo, vai=Fruta.bota)
 NameError: name 'Fruta' is not defined
 '''},
+{'date': 'Mon Aug 31 2020 20:29:04.414 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    Ambiente()
+  module <module> line 42
+    self.maca=Elemento(ITEM["MACA"], tit="maçã",style=dict(height=60,widht=60, left=600, top=20),drag=True,drop={},cena=self.fundo, vai=Item.bota(self.maca))
+AttributeError: 'Ambiente' object has no attribute 'maca'
+'''},
