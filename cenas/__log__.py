e12dd04084e446aa11fd9d7db15b94caa8b1f398
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-31 17:09:44 -0300

    Saved cenas/__log__.py @2020-08-31 20:09

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1533,3 +1533,21 @@ AttributeError: 'ambiente' object has no attribute 'maca'
                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 31 2020 17:09:44.102 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    ambiente()
+  module <module> line 30
+    self.maca=Elemento(ITEM["MACA"], tit="maçã", txt="Opa, você encontrou uma maçã!", h=100,w=100, x=100, y=20, cena=self.fundo, vai=tenta)
+NameError: name 'tenta' is not defined
+'''},
