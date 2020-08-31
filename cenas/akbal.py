9e96251314305aa4763f6b66f8e5b395662c2eb2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-30 22:39:45 -0300

    Created cenas/akbal.py @2020-08-31 01:39

---
+++ cenas/akbal.py
@@ -0,0 +1,22 @@
+# janese.cenas.akbal.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Vittolino na UNESP.
+
+.. codeauthor:: Equipe Supygirls <supygirls@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.08
+      - Criação de Elementos
+      - Implementação do inventário
+
+"""
+from _spy.vitollino.main import Cena, Elemento
+from _spy.vitollino.main import Inventario as inv
+
+ITEM={"LARANJA":"https://i.imgur.com/XXi1abd.png",
+      "MACA":"https://i.imgur.com/8aCEBLc.png",
+      "FLORESTA":"https://i.imgur.com/xUzMidi.jpeg"}
+
+
+class ambiente():
