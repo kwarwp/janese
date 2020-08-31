96d660b13c810bc6aa4464c950619888b43fc27a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-31 17:40:07 -0300

    Saved cenas/akbal.py @2020-08-31 20:40

--- cenas/akbal.py
+++ cenas/akbal.py
@@ -27,16 +27,14 @@ class ambiente():
     def __init__(self):
         inv.inicia()
         self.fundo=Cena(ITEM["FLORESTA"])
-        self.maca=Elemento(ITEM["MACA"], tit="maçã", txt="Opa, você encontrou uma maçã!", 
-                          style={h=100,w=100, x=100, y=20},
-                          cena=self.fundo, vai=teste_inventario())
-        self.laranja=Elemento(ITEM["LARANJA"], tit="laranja", txt="Opa, você encontrou uma laranja!",style={h=100,w=100, x=100, y=120},
+        self.maca=Elemento(ITEM["MACA"], tit="maçã", texto="Opa, você encontrou uma maçã!", h=100,w=100, x=100, y=20, cena=self.fundo, vai=self.teste_inventario)
+        self.laranja=Elemento(ITEM["LARANJA"], tit="laranja", txt="Opa, você encontrou uma laranja!",style=dict(h=100,w=100, x=100, y=120),
                               cena=self.fundo)
         inv.bota(self.laranja, drag=True)
         self.fundo.vai()
         
     def teste_inventario(self):
-        return inv.bota(self.maca, True)
+        return inv.bota(self.maca)
         
 if __name__ == "__main__":
     ambiente()
