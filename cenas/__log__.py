b8a6055b6a9029764f9c03cad951755342a39633
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-31 17:01:04 -0300

    Saved cenas/__log__.py @2020-08-31 20:01

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1418,3 +1418,21 @@ AttributeError: 'method' object has no attribute 'cenas'
     self.centro.cenas[indice].portal(N=sala.cenas[indice]) if sala else None
 AttributeError: 'method' object has no attribute 'cenas'
 '''},
+{'date': 'Mon Aug 31 2020 17:01:03.898 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    ambiente()
+  module <module> line 32
+    inv.bota(maca)
+NameError: name 'maca' is not defined
+'''},
