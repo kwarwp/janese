271b8a118f98631450d1d29a21c88f5024727ad7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-31 17:07:30 -0300

    Saved cenas/__log__.py @2020-08-31 20:07

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1508,3 +1508,21 @@ TypeError: bota missing 1 positional argument: 'nome_item'
     self.maca=Elemento(ITEM["MACA"], tit="maçã", txt="Opa, você encontrou uma maçã!", h=100,w=100, x=100, y=20, cena=self.fundo, vai=tenta)
 UnboundLocalError: local variable 'tenta' referenced before assignment
 '''},
+{'date': 'Mon Aug 31 2020 17:07:29.902 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    ambiente()
+  module <module> line 29
+    tenta = inv.bota(self.maca)
+AttributeError: 'ambiente' object has no attribute 'maca'
+'''},
