6135b121b22ecf27b800e2522bd20354885d4fad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 19:24:03 -0300

    Saved cenas/__log__.py @2020-09-03 22:24

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1887,3 +1887,21 @@ NameError: name 'Fruta' is not defined
     self.maca=Elemento(ITEM["MACA"], tit="maçã",style=dict(height=60,widht=60, left=600, top=20),drag=True,drop={},cena=self.fundo, vai=Item.bota(self.maca))
 AttributeError: 'Ambiente' object has no attribute 'maca'
 '''},
+{'date': 'Thu Sep 03 2020 19:24:01.979 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    Ambiente()
+  module <module> line 42
+    self.maca=Elemento(ITEM["MACA"], tit="maçã",style=dict(height=60,widht=60, left=600, top=20),drag=True,drop={},cena=self.fundo, vai=guarda_item)
+NameError: name 'guarda_item' is not defined
+'''},
