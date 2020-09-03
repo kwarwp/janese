bddd9e41a74936574302755683b2a3dc32755108
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 19:38:00 -0300

    Saved cenas/__log__.py @2020-09-03 22:38

--- cenas/__log__.py
+++ cenas/__log__.py
@@ -1927,3 +1927,25 @@ NameError: name 'guarda_item' is not defined
     self.centro.cenas[indice].portal(N=sala.cenas[indice]) if sala else None
 AttributeError: 'method' object has no attribute 'cenas'
 '''},
+{'date': 'Thu Sep 03 2020 19:37:59.109 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 61
+    Passeio()
+  module <module> line 39
+    self.colecao_total=Labirinto(n=self.sala_um,c=self.sala_dois)
+  module _spy.vitollino.main line 926
+    self.lb()
+  module _spy.vitollino.main line 930
+    self.centro.cenas[indice].portal(N=sala.cenas[indice]) if sala else None
+AttributeError: 'method' object has no attribute 'cenas'
+'''},
