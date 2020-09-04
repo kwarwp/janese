7f2a3841d4a7c8faf0d1b2dd8ceb675bfdf95de4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 21:54:34 -0300

    Saved cenas/akbal.py @2020-09-04 00:54

--- cenas/akbal.py
+++ cenas/akbal.py
@@ -26,16 +26,17 @@ class Item(Elemento):
     #self.elemento = None
     def bota(self, *_): 
         inv.bota(self, True)
-        x,y,w,h=self.memento
-        self.vai=lambda*_:self.resgata(x=x,y=y,w=w,h=h)
-        
+        #x,y,w,h=self.memento
+        #self.vai=lambda*_:self.resgata(x=x,y=y,w=w,h=h)
+        self.vai=lambda*_:self.resgata(*self.memento)
+    
     def resgata(self,x,y,w,h):
-        self.x,self.y,self.w,self.h=self
+        self.x,self.y,self.w,self.h= x,y,w,h
         inv.tira(self.tit)
         self.entra(inv.cena)
         self.vai=self.bota
         
-    def memento(self,memento):
+    def mementor(self,memento):
         self.memento=memento
         
     
@@ -49,7 +50,7 @@ class Ambiente():
         self.maca=Elemento(ITEM["MACA"], tit="maçã",style=dict(height=60,widht=60, left=600, top=20),cena=self.fundo, vai=self.guarda_item)
         
         self.laranja=Item(ITEM["LARANJA"], tit="laranja",style=dict(height=60,widht=60, left=100, top=100),cena=self.fundo)
-        self.laranja.memento(110,150,100,100)
+        self.laranja.mementor((110,150,100,100))
         self.laranja.vai=self.laranja.bota
         
 
