ea8b472637950da96aaf33ee6112e6e69dd45562
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-07 20:17:16 -0300

    Saved kristen/main.py @2020-09-07 23:17

--- kristen/main.py
+++ kristen/main.py
@@ -1,9 +1,7 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
 FUNDO = "https://imgur.com/cf9dvHG.png"
-BONEQUINHA = "https://imgur.com/gfe9a1S.png"
-BOTAO10 = "https://imgur.com/7JoDeNt.png"
-BOTAO20 = "https://imgur.com/ORgkcss.png"
+BONEQUINHA = "https://imgur.com/Z8SiiTg.png"
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
@@ -13,26 +11,24 @@ class inicialesquerda():
 
     def __init__(self):
         self.fundo = Cena(FUNDO)
-        self.a = Texto(self.fundo, txt = "Descubra a função que levará a Profa até o topo da montanha") 
-        self.a.vai()
-        self.bonequinha = bonequinha(self.fundo, self.a)
+        self.bonequinha = bonequinha(self.fundo)
+        self.mais = Texto(self.fundo, txt = "Clique aqui") 
         
-                
+         
     def vai(self):
         self.fundo.vai()
                
 class bonequinha():
-    def __init__(self,fundo,a):
-        self.boneca = Elemento(BONEQUINHA, texto = "Voe!", h=250 , w=250, x=150, y=450)
-        self.x = 150
-        self.c = 100
-       # self.a = a
+    def __init__(self,fundo):
+        self.boneca = Elemento(BONEQUINHA, texto = "Voe!", h=150 , w=250, x=50, y=200)
+        self.x = 50
+        self.c = 200
         self.boneca.vai = self.equacao1
         self.boneca.entra(fundo)
         
     def equacao1(self,*_):
-        self.boneca.x = self.x = self.x+10
-        self.boneca.y = self.x*(-10) + self.c
+        self.boneca.x = self.x+10
+        self.boneca.y = self.a*2+self.c
     
 if __name__ == "__main__":  
 	inicialesquerda().vai()
