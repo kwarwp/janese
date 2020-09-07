b6d693b78f186bb8bc05a04728ccf7a6d441a2b3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-07 20:24:44 -0300

    Saved kristen/main.py @2020-09-07 23:24

--- kristen/main.py
+++ kristen/main.py
@@ -1,26 +1,27 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
 FUNDO = "https://imgur.com/cf9dvHG.png"
-BONEQUINHA = "https://imgur.com/Z8SiiTg.png"
+BONEQUINHA = "https://imgur.com/gfe9a1S.png"
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
 
 class inicialesquerda():
-
     def __init__(self):
         self.fundo = Cena(FUNDO)
-        self.bonequinha = bonequinha(self.fundo)
-        self.mais = Texto(self.fundo, txt = "Clique aqui") 
-        
+        #self.bonequinha = bonequinha(self.fundo)
+        self.mais = Texto(self.fundo, txt = "Clique aqui", foi=self.foi, A="10", B="20") 
+        self.cf = dict(A=10, B=20) 
+    def foi(self, opcao)
+        self.bonequinha = bonequinha(self.fundo, self.cf[opcao])
          
     def vai(self):
         self.fundo.vai()
                
 class bonequinha():
     def __init__(self,fundo):
-        self.boneca = Elemento(BONEQUINHA, texto = "Voe!", h=150 , w=250, x=50, y=200)
+        self.boneca = Elemento(BONEQUINHA, texto = "Voe!", h=250 , w=250, x=50, y=200)
         self.x = 50
         self.c = 200
         self.boneca.vai = self.equacao1
