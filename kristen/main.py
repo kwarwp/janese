19c14757bf1aafce509ebb2b3cd4155067ee78ee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-07 20:41:45 -0300

    Saved kristen/main.py @2020-09-07 23:41

--- kristen/main.py
+++ kristen/main.py
@@ -11,7 +11,7 @@ STYLE["heigth"] = 900
 class inicialesquerda():
     def __init__(self):
         self.fundo = Cena(FUNDO)
-        self.zero = Elemento (INICIOMONTANHA, x=300 , y=700, cena=self.fundo)
+        self.zero = Elemento (INICIOMONTANHA, x=300 , y=500, cena=self.fundo)
         #self.bonequinha = bonequinha(self.fundo)
         self.mais = Texto(self.fundo, txt = "Clique aqui", foi=self.foi, A="2,5", B="-2,5")
         self.mais.vai()
