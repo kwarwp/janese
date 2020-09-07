d0a230b5f36596cae54430f778f58c494375efc1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-07 17:22:46 -0300

    Saved kristen/main.py @2020-09-07 20:22

--- kristen/main.py
+++ kristen/main.py
@@ -1,6 +1,6 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
-FUNDO = "[img]https://i.imgur.com/CrjHmQY.png[/img]"
+FUNDO = "https://imgur.com/CrjHmQY"
 LIVRO = "https://oamor.com.br/wp-content/uploads/2018/12/Quando-a-borboleta-rompe-o-casulo-2-830x450.jpg"
 
 STYLE["width"] = 900
