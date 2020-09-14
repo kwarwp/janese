54e0cc779143c5231f93c32e1cf08ebfb9072521
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:39:37 -0300

    Saved samantha/__log__.py @2020-09-14 23:39

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -451,3 +451,21 @@ SyntaxError: invalid syntax
     self.zero = Elemento(INCIO, x=230, y=620, cena = self.fundo, vai = self.comeca)
 NameError: name 'INCIO' is not defined
 '''},
+{'date': 'Mon Sep 14 2020 20:39:36.532 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 53
+    	inicio().vai()
+  module <module> line 14
+    self.boneco = boneco(self.fundo, 1)
+TypeError: __init__() missing 1 positional argument: opcao2
+'''},
