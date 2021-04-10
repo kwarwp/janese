5b152cfb392896ffe3151e30fcc1eddcb44b9989
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-10 15:48:16 -0300

    Saved anastasia/__log__.py @2021-04-10 18:48

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -6,3 +6,10 @@
                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Apr 10 2021 15:48:15.748 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  if __name__ = "__main__":
+               ^
+SyntaxError: invalid syntax
+'''},
