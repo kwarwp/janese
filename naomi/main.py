d5cae459b289a5616be190834941313bc626fee5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-10 15:43:08 -0300

    Created naomi/main.py @2021-04-10 18:43

---
+++ naomi/main.py
@@ -0,0 +1,17 @@
+# janese.naomi.main.py
+#Samantha aqui
+
+from circus.circus import circus
+
+TOPO_ESQUERDA = "AN"
+TOPO_DIREITA = "AN"
+TOPO_CENTRO = "AN"
+MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+
+# O comando abaixo voce vai entender no próximo desafio
+circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+        MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
+        
+if __name__ == "__main__":
+desafio()
