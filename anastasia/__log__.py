8fab65acc3f315e604924e77b77c03843ad03755
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-10 15:48:03 -0300

    Created anastasia/__log__.py @2021-04-10 18:48

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Apr 10 2021 15:48:00.895 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  def desafio0:
+               ^
+SyntaxError: invalid syntax
+'''},
