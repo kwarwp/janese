4693fbf8d48c5b57019e89a233d6a212579b8e68
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-10 15:51:23 -0300

    Saved roxanne/__log__.py @2021-04-10 18:51

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -26,3 +26,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sat Apr 10 2021 15:51:24.181 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  TOPO_ESQUERDA = "LS"
+  ^
+IndentationError: expected an indented block
+'''},
