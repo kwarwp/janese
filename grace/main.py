05c9674d5346e55136227e81416dbf82f0e3665d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-10 15:38:22 -0300

    Saved grace/main.py @2021-04-10 18:38

--- grace/main.py
+++ grace/main.py
@@ -3,7 +3,7 @@
 #from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
 
-from _circus.circus import circus
+from circus.circus import circus
 def desafio0():
     TOPO_ESQUERDA = "AN"
     TOPO_DIREITA = "AN"
@@ -12,8 +12,7 @@ def desafio0():
     FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
 
 # O comando abaixo voce vai entender no próximo desafio
-circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
-MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
+circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO, MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
 
 if __name__=="__main__":
     desafio0()
