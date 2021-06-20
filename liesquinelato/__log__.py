8c9c12bfc570826f77eb4f0720a8021b938e2f21
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 16:26:23 -0300

    Saved liesquinelato/__log__.py @2021-06-20 19:26

--- liesquinelato/__log__.py
+++ liesquinelato/__log__.py
@@ -88,3 +88,25 @@ SyntaxError: invalid syntax
     self.fundo = Cena(self.FUNDO)
 AttributeError: 'inicial' object has no attribute 'FUNDO'
 '''},
+{'date': 'Sun Jun 20 2021 16:26:19.928 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 20
+    inicial().vai()     
+  module <module> line 14
+    self.fundo = Cena(FUNDO, w=900, h=900)
+  module _spy.vitollino.main line 1095
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 1138
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+AttributeError: 'int' object has no attribute '__getitem__'
+'''},
