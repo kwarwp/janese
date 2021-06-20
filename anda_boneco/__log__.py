143cee43de42d0791fa05f5081455086552beedc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 16:50:29 -0300

    Created anda_boneco/__log__.py @2021-06-20 19:50

---
+++ anda_boneco/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Jun 20 2021 16:50:44.225 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  self.cf = dict(A = "(2,3)"
+                                  ^
+SyntaxError: invalid syntax
+'''},
