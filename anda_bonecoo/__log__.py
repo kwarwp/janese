e2a853102f0413c27971baf5b5d98be92e67d360
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 17:20:07 -0300

    Saved anda_bonecoo/__log__.py @2021-06-20 20:20

--- anda_bonecoo/__log__.py
+++ anda_bonecoo/__log__.py
@@ -55,3 +55,21 @@ RuntimeError: too much recursion
   module '$exec_1857' line 26
     Cena().vai()
 '''},
+{'date': 'Sun Jun 20 2021 17:20:28.624 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 26
+    Cena().vai()
+  module <module> line 13
+    self.fundo = Cena(FUNDO)
+TypeError: __init__() takes 1 positional argument but more were given
+'''},
