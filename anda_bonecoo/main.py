c1cdde787bcb6b9326d8a066acc8858962fe98f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 18:23:00 -0300

    Saved anda_bonecoo/main.py @2021-06-20 21:23

--- anda_bonecoo/main.py
+++ anda_bonecoo/main.py
@@ -2,7 +2,7 @@
 
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
-BONEQUINHA = "https://imgur.com/gfe9a1S.png"
+PERSONAGEM = "https://imgur.com/gfe9a1S.png"
 FUNDO = "https://imgur.com/vaUUL1h.jpg"
 MARCADOR_ESQUERDA = "https://imgur.com/hEF8XPG.png"
 MARCADOR_DIREITA = "https://imgur.com/hEF8XPG.png"
@@ -20,27 +20,24 @@ class Quadro:
         self.bonequinha = bonequinha(self.fundo)
         self.comeca()
         
-    def comeca(self, *_):
-        self.bonequinha.x = 200
-        self.mais = Texto(self.fundo, txt = "Clique aqui", foi=self.foi, A="-1,3655", B="-4")
-        self.mais.vai()
-    
-        
-    def foi(self,opcao):
-        self.bonequinha.a = self.cf [opcao]
-        self.bonequinha.x = 200
-        self.bonequinha.y = 620
-        self.bonequinha.aparece()
-        self.cf = dict(A = "(2,3)")
-
     def vai(self):
         self.fundo.vai()
 
-class Joystick:
+class Persona_control:
+""" Constrói um boneco que se movimenta com clique no joystick
 
-    def __init__(self):
-        pass
-        
+nome_do_elemento = Elemento(MEU_ELEMENTO, 
+                            h=xx , w=xx,
+                            x=xx, y=xx, cena= nome_da_cena, vai=funcao)    
+    
+"""
+    def __init__(self, fundo, ):
+        self.x = x
+        self.y = y
+        self.persona = Elemento(PERSONAGEM, h=250 , w=250, x=self.x, y=self.y)
+        self.boneca.vai = self.equacao1
+        self.persona.entra(fundo)
+
     def anda():
         pass
         
