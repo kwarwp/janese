b0bf0272ac7c3bd437269392b7839c86101b5926
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 17:03:04 -0300

    Saved anda_boneco/__log__.py @2021-06-20 20:03

--- anda_boneco/__log__.py
+++ anda_boneco/__log__.py
@@ -49,3 +49,21 @@ NameError: name 'FUNDO' is not defined
     self.bonequinha = bonequinha(self.fundo)
 TypeError: __init__() missing 1 positional argument: opcao
 '''},
+{'date': 'Sun Jun 20 2021 17:03:22.297 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 62
+    inicialesquerda().vai()
+  module <module> line 21
+    self.bonequinha = bonequinha(self.fundo)
+TypeError: __init__() missing 1 positional argument: opcao
+'''},
