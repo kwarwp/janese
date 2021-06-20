b74c67f86e40ef0249c19221ede28131e9d77590
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 17:17:28 -0300

    Created anda_bonecoo/main.py @2021-06-20 20:17

---
+++ anda_bonecoo/main.py
@@ -0,0 +1,26 @@
+# janese.anda_bonecoo.main.py
+from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+
+BONEQUINHA = "https://imgur.com/gfe9a1S.png"
+FUNDO = "https://imgur.com/vaUUL1h.jpg"
+MARCADOR = "https://imgur.com/hEF8XPG.png"
+
+STYLE["width"] = 900
+STYLE["heigth"] = 900
+
+class cena():
+    def __init__ ():
+        self.fundo = Cena(FUNDO)
+        
+    def vai(self):
+        self.fundo.vai()
+        
+class personagem():
+    def __init__():
+        pass
+        
+    def andar():
+        pass
+        
+if __name__ == "__main__":  
+    cena().vai()
