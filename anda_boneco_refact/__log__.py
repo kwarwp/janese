7225b7a39c904376053b57caa30bd81759828a9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 20:10:12 -0300

    Saved anda_boneco_refact/__log__.py @2021-06-20 23:10

--- anda_boneco_refact/__log__.py
+++ anda_boneco_refact/__log__.py
@@ -44,3 +44,23 @@ SyntaxError: duplicate argument 'self' in function definition
     movi = self.movi 
 AttributeError: 'Persona_control' object has no attribute 'movi'
 '''},
+{'date': 'Sun Jun 20 2021 20:11:01.174 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 77
+    Quadro().vai()
+  module <module> line 21
+    self.bonequinha = Persona_control(self.fundo,300)        
+  module <module> line 44
+    self.persona = Elemento(PERSONAGEM, h=100 , w=100, x=self.x, y=self.y)
+NameError: name 'PERSONAGEM' is not defined
+'''},
