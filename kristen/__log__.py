1b6ba0423ae5157ba85cc2b0512016fc5460da6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 16:37:23 -0300

    Saved kristen/__log__.py @2021-06-20 19:37

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -140,3 +140,10 @@ SyntaxError: invalid syntax
                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Jun 20 2021 16:37:20.802 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  self.cf = dict(A = "(2,3)"
+                                  ^
+SyntaxError: invalid syntax
+'''},
