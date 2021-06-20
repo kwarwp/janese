73a99f85e169aab9111fcc88971f71a6b971d664
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 16:36:09 -0300

    Saved kristen/__log__.py @2021-06-20 19:36

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -108,3 +108,21 @@ NameError: name 'INICIOMONTANHA' is not defined
     self.zero = Elemento (INICIOMONTANHA, x=230 , y=620, cena=self.fundo, vai=self.comeca)
 NameError: name 'INICIOMONTANHA' is not defined
 '''},
+{'date': 'Sun Jun 20 2021 16:36:06.514 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 61
+    	inicialesquerda().vai()
+  module <module> line 17
+    self.fundo = Cena(FUNDO)
+NameError: name 'FUNDO' is not defined
+'''},
