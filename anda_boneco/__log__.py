a7d0915933f419bac42878dee8933031aa14b392
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 16:50:31 -0300

    Saved anda_boneco/__log__.py @2021-06-20 19:50

--- anda_boneco/__log__.py
+++ anda_boneco/__log__.py
@@ -6,3 +6,10 @@
                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Jun 20 2021 16:50:47.672 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  self.cf = dict(A = "(2,3)"
+                                  ^
+SyntaxError: invalid syntax
+'''},
