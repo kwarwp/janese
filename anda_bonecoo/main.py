6cd17584d05afdd4ae625dce37dc725747869603
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 19:37:11 -0300

    Saved anda_bonecoo/main.py @2021-06-20 22:37

--- anda_bonecoo/main.py
+++ anda_bonecoo/main.py
@@ -14,21 +14,17 @@ STYLE["width"] = 900
 STYLE["heigth"] = 900
 
 class Quadro:
+
     def __init__(self,*_):   
         self.fundo = Cena(FUNDO) 
         self.bonequinha = Persona_control(self.fundo)        
-
-        #self.cima = Elemento(PERSONAGEM, h=100 , w=100, x=10, y=430, cena= self.fundo)
-        #self.cima = Elemento(MARCADOR_CIMA, h=40 , w=40, x=780, y=450, cena= self.fundo)
-        #self.baixo = Elemento(MARCADOR_BAIXO, h=40 , w=40, x=780, y=530, cena= self.fundo)
-        #self.direita = Elemento(MARCADOR_DIREITA, h=40 , w=40, x=720, y=490, cena= self.fundo)
-        #self.esquerda = Elemento(MARCADOR_ESQUERDA, h=40 , w=40, x=840, y=490, cena= self.fundo)
-
         
     def vai(self):
         self.fundo.vai()
         
 class Persona_control:
+    """ Cria um elemento que anda a partir do clique em outro elemento.
+    """
 
     def __init__(self, fundo):
         self.x = 10
@@ -37,8 +33,8 @@ class Persona_control:
         self.persona = Elemento(PERSONAGEM, h=100 , w=100, x=self.x, y=self.y)
         self.persona.entra(fundo)
         
-        self.persona.x = self.x
-        self.persona.y = self.y
+        #self.persona.x = self.x
+        #self.persona.y = self.y
         
         
         self.cima = Elemento(MARCADOR_CIMA, h=40 , w=40, x=780, y=450, vai=self.anda_cima)
@@ -54,8 +50,9 @@ class Persona_control:
         self.esquerda.entra(fundo)
 
     def anda_direita(self,*_):
+        """
+        """
         self.persona.x = self.x = self.x - 10
-
     def anda_esquerda(self,*_):
         self.persona.x = self.x = self.x + 10
         
