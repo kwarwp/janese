40ccb82362b830ad10c0456e299e9e11dd5751cb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-20 16:37:10 -0300

    Saved kristen/__log__.py @2021-06-20 19:37

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -133,3 +133,10 @@ NameError: name 'FUNDO' is not defined
                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Jun 20 2021 16:37:07.21 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  self.cf = dict(A = "(2,3)"
+                                  ^
+SyntaxError: invalid syntax
+'''},
