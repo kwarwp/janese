a9481582be1d76b7b4a73941176c63fe44928d04
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-21 17:36:40 -0300

    Saved anastasia/main.py @2021-06-21 20:36

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,1 +1,10 @@
 #Profa Gi :)
+
+from _spy.vitolino.main import Cena, Elemento, STYLE
+from browser import document
+FUNDO = "https://imgur.com/bg1h3oD"
+PERSONAGEM = "https://imgur.com/7qQzMJb"
+
+class inicial():
+ def __init__(self):
+ 
