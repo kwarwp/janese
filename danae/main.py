42ae3d8ccec6a46512b4fb19a0abb583586780f2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-21 17:36:32 -0300

    Saved danae/main.py @2021-06-21 20:36

--- danae/main.py
+++ danae/main.py
@@ -1,2 +1,21 @@
 # janese.danae.main.py
 #Lilian Esquinelato
+
+from _spy.vitollino.main import Cena, Elemento, STYLE
+from browser import document 
+
+FUNDO = "https://i.imgur.com/73J8HqZ.png"
+PERSONAGEM = "https://imgur.com/0Zyn3IL.png"
+
+class inicial():
+    def __init__(self):
+        self.fundo = Cena(FUNDO)
+        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo)
+        
+    def vai(self):
+        self.fundo.vai()
+        
+        
+        
+if __name__ == "__main__":
+    inicial().vai()
