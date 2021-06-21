6c41f6a7385bcea07242c8f1b706913dc242017a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-21 17:40:51 -0300

    Saved anastasia/main.py @2021-06-21 20:40

--- anastasia/main.py
+++ anastasia/main.py
@@ -6,5 +6,13 @@ FUNDO = "https://imgur.com/bg1h3oD"
 PERSONAGEM = "https://imgur.com/7qQzMJb"
 
 class inicial():
- def __init__(self):
- 
+    def __init__(self):
+        self.fundo = Cena(FUNDO)
+        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo)
+        
+    def vai(self):
+        self.fundo.vai()
+        
+        
+if __name__ == "__manin__":
+    inicial().vai()
