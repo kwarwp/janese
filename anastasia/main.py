443a0d2af7e0e21a269ccd242f79e16b95cfd9bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-21 17:42:43 -0300

    Saved anastasia/main.py @2021-06-21 20:42

--- anastasia/main.py
+++ anastasia/main.py
@@ -3,8 +3,8 @@
 from _spy.vitolino.main import Cena, Elemento, STYLE
 from browser import document
 
-FUNDO = "https://imgur.com/bg1h3oD"
-PERSONAGEM = "https://imgur.com/7qQzMJb"
+FUNDO = "https://imgur.com/bg1h3oD.png"
+PERSONAGEM = "https://imgur.com/7qQzMJb.png"
 
 class inicial():
     def __init__(self):
