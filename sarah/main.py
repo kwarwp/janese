b607f79a7dacb6e0cbca775f8a7f82ab9d9cc886
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-21 17:48:14 -0300

    Saved sarah/main.py @2021-06-21 20:48

--- sarah/main.py
+++ sarah/main.py
@@ -1,40 +1,24 @@
 # janese.sarah.main.py
-from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-from cenas.imix import Inicial
+#Helena Pacheco
 
-FUNDO = "https://img.freepik.com/vetores-gratis/fundo-branco-textura-elegante_23-2148415643.jpg?size=626&ext=jpg"
-CALEND={"IMIX":"https://i.imgur.com/XmmzDHZ.png", 
-        "IK":"https://i.imgur.com/9nieFUZ.png",
-        "AKBAL":"XX",
-        "KAN":"XX",
-        "CHICCHAN":"XX",
-        "CIMI":"XX",
-        "MANIK":"XX",
-        "LAMAT":"XX",
-        "MALUC":"XX",
-        "OC":"XX",
-        "CHUEN":"XX",
-        "EB":"XX",
-        "AKBAL":"XX"
-       }
-       
-STYLE["width"] = 900
-STYLE["heigth"] = 900
+from_spy.vitollino.main import Cena, Elemento, STYLE
+from brownser import document
+
+FUNDO = "https://imgur.com/gallery/SJ4CR6x"
+PERSONAGEM = "https://imgur.com/gallery/kiO6u4F"
+
+STYLE["wedth"]=500
+STYLE["height"= 200
+
+class inicial():
+def __int__(self):
+self.fundo= cena(FUNDO)
+self.personagem=Elemento(img=PERSONAGEM, cena=self.fundo)
+
+def vai(self):
+self.fundo.vai()
 
 
-class Estrutura():
 
-
-    def __init__(self):
-    
-        self.fundo=Cena(FUNDO)
-        self.fundo.vai()
-        """ Primeiro modo de criar um botão. Utiliza a função vai() do vitollino"""
-        self.imix=Elemento(CALEND["IMIX"], tit="dia 17/08/2020", x=0, y=100, w=300, h=300, cena=self.fundo, vai=self.botao_17082020)
-
-    def botao_17082020(self, event=None):
-        """ Método que permite a instância do botão."""
-        Inicial().chama()
-            
-if __name__ == "__main__":
-    Estrutura()
+if __name__== "__main__":
+inicial().vai()
