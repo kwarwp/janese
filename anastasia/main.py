7dd2e1a6bc09c1074acea3b65d356f5cecba93e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-21 17:47:12 -0300

    Saved anastasia/main.py @2021-06-21 20:47

--- anastasia/main.py
+++ anastasia/main.py
@@ -6,14 +6,21 @@ from browser import document
 FUNDO = "https://imgur.com/bg1h3oD.png"
 PERSONAGEM = "https://imgur.com/7qQzMJb.png"
 
+STYLE["width"]=500
+STYLE["height"]=120
+
 class inicial():
     def __init__(self):
         self.fundo = Cena(FUNDO)
-        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo)
+        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=0, y=500)
+        document.bind("keydown", self.andapersonagem)
+        
         
     def vai(self):
         self.fundo.vai()
         
+    #def andapersonagem(self, ev=None):
         
-if __name__ == "__manin__":
+        
+if __name__ == "__main__":
     inicial().vai()
