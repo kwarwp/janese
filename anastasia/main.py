5b11f7738c5f934822d64574bdf73a621767d56f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-21 17:52:24 -0300

    Saved anastasia/main.py @2021-06-21 20:52

--- anastasia/main.py
+++ anastasia/main.py
@@ -12,14 +12,23 @@ PERSONAGEM = "https://imgur.com/7qQzMJb.png"
 class inicial():
     def __init__(self):
         self.fundo = Cena(FUNDO)
-        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=0, y=500)
+        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo)
         document.bind("keydown", self.andapersonagem)
         
         
     def vai(self):
         self.fundo.vai()
         
-    #def andapersonagem(self, ev=None):
+    def andapersonagem(self, ev=None):
+        teclado = ev.keyCode
+        
+        if teclado in [37,39]:
+            teclado = (teclado - 38) * 10
+            self.personagem.x = self.personagem.x + teclado
+            
+        elif teclado in [38, 40]:
+            teclado = (teclado - 39) * 10
+            self.personagem.y = self.personagem.y + teclado
         
         
 if __name__ == "__main__":
