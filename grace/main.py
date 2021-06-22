d82f51143fa1c4f3670f13d832c8d4c7b6fc8145
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-21 21:58:42 -0300

    Saved grace/main.py @2021-06-22 00:58

--- grace/main.py
+++ grace/main.py
@@ -1,20 +1,40 @@
 # janese.grace.py
-# SPDX-License-Identifier: GPL-3.0-or-later
-#from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+# Bianca
+
+from _spy.vitollino.main import Cena, Elemento, STYLE
+from browser import document 
+
+FUNDO = "https://i.imgur.com/73J8HqZ.png"
+PERSONAGEM = "https://imgur.com/0Zyn3IL.png"
+
+STYLE["width"]=500
+STYLE["height"]=120
+
+class inicial():
+    def init(self):
+        self.x1 = 0
+        self.y1 = 500
+        self.fundo = Cena(FUNDO)
+        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=50, w=50)
+        document.bind("keydown", self.andapersonagem)
+
+    def vai(self):
+        self.fundo.vai()
+
+    def andapersonagem(self, ev=None):
+        teclado = ev.keyCode
+
+        if teclado in [37, 39]:
+            teclado = (teclado - 38) * 10 #podem modificar o valor do 10
+            self.personagem.x = self.personagem.x + teclado
+
+        elif teclado in [38, 40]:
+            teclado = (teclado - 39) * 10
+            self.personagem.y = self.personagem.y + teclado
 
 
-from circus.circus import circus
-def desafio0():
 
-    TOPO_ESQUERDA = "AN"
-    TOPO_DIREITA = "AN"
-    TOPO_CENTRO = "AN"
-    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
-    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+if name == "main":
+    inicial().vai()
 
-# O comando abaixo voce vai entender no próximo desafio
-    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
-        MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
-        
-if __name__ == "__main__":
-    desafio0()
+   
