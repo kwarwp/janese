2107b6c0c2bd6cb80f1883f97aff520a20892489
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 17:19:11 -0300

    Saved sarah/main.py @2021-07-05 20:19

--- sarah/main.py
+++ sarah/main.py
@@ -11,15 +11,28 @@ STYLE["wedth"]=500
 STYLE["height"]= 200
 
 class inicial():
-  def init(self):
+    def init(self):
         self.x1 = 0
         self.y1 = 500
         self.fundo = Cena(FUNDO)
-self.personagem=Elemento(img=PERSONAGEM, cena=self.fundo)
+        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=50, w=50)
+        document.bind("keydown", self.andapersonagem)
 
-  def vai(self):
-  self.fundo.vai()
+    def vai(self):
+        self.fundo.vai()
+
+    def andapersonagem(self, ev=None):
+        teclado = ev.keyCode
+
+        if teclado in [37, 39]:
+            teclado = (teclado - 38) * 10 #podem modificar o valor do 10
+            self.personagem.x = self.personagem.x + teclado
+
+        elif teclado in [38, 40]:
+            teclado = (teclado - 39) * 10
+            self.personagem.y = self.personagem.y + teclado
 
 
-if __name__== "__main__":
-inicial().vai()
+
+if name == "main":
+    inicial().vai()
