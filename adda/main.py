4165d15cd181e93619de20bf81b9b5dcc0b9091a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 17:50:12 -0300

    Saved adda/main.py @2021-07-05 20:50

--- adda/main.py
+++ adda/main.py
@@ -6,7 +6,8 @@ from _spy.vitollino.main import Cena, Elemento, STYLE
 from browser import document
 
 FUNDO = "https://i.imgur.com/KYKStgT.png"
-PERSONAGEM = "https://i.imgur.com/Jo5Ho94.png"
+PERSONAGEM1 = "https://i.imgur.com/Sr5zj5S.png"
+PERSONAGEM2 = "https://i.imgur.com/Jo5Ho94.png"
 
 STYLE["width"]=1150
 STYLE["height"]=520
@@ -15,11 +16,12 @@ class inicial():
 
     def __init__(self):
         self.x1 = 0
-        self.y1 = 440
+        self.y1 = 460
         self.fundo = Cena(FUNDO)
-        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
+        self.personagem1 = Elemento(img = PERSONAGEM1, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
         document.bind("keydown", self.andapersonagem)
-        
+        self.personagem2 = Elemento(img = PERSONAGEM2, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
+        document.bind("keydown", self.andapersonagem)
         
     def vai(self):
         self.fundo.vai()
@@ -29,11 +31,11 @@ class inicial():
         
         if teclado in [37,39]:
             teclado = (teclado - 38) * 10
-            self.personagem.x = self.personagem.x + teclado
+            self.personagem2.x = self.personagem2.x + teclado
             
         elif teclado in [38, 40]:
             teclado = (teclado - 39) * 10
-            self.personagem.y = self.personagem.y + teclado
+            self.personagem2.y = self.personagem2.y + teclado
         
         
 if __name__ == "__main__":
