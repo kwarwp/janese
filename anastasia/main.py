218313274c26a6eec4399214ea8a1da46545f2f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 17:06:37 -0300

    Saved anastasia/main.py @2021-07-05 20:06

--- anastasia/main.py
+++ anastasia/main.py
@@ -16,6 +16,7 @@ class inicial():
         self.y1 = 500
         self.fundo = Cena(FUNDO)
         self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=50, w=50)
+        self.personagem.style()
         document.bind("keydown", self.andapersonagem)
         
         
