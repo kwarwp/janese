921413dd5d834dc61e57693dca5bf28252ae7681
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 17:00:02 -0300

    Saved anastasia/main.py @2021-07-05 20:00

--- anastasia/main.py
+++ anastasia/main.py
@@ -6,7 +6,7 @@ from browser import document
 FUNDO = "https://imgur.com/bg1h3oD.png"
 PERSONAGEM = "https://imgur.com/7qQzMJb.png"
 
-STYLE["width"]=500
+STYLE["width"]=700
 STYLE["height"]=120
 
 class inicial():
