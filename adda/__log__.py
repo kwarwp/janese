7e4cfeab5feb7b17c2de960993d0c3f0079ad201
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-04 22:26:04 -0300

    Created adda/__log__.py @2021-07-05 01:26

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Jul 04 2021 22:26:02.577 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  PERSONAGEM ANDANDO= "https://i.imgur.com/Jo5Ho94.png"
+              ^
+SyntaxError: invalid syntax
+'''},
