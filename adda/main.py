ced7243552792c24ea6bdc7efed34a97f16bca2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-04 21:41:47 -0300

    Saved adda/main.py @2021-07-05 00:41

--- adda/main.py
+++ adda/main.py
@@ -8,8 +8,8 @@ from browser import document
 FUNDO = "https://i.imgur.com/KYKStgT.png"
 PERSONAGEM = "https://imgur.com/7qQzMJb.png"
 
-STYLE["width"]=1920
-STYLE["height"]=1080
+STYLE["width"]=640
+STYLE["height"]=360
 
 class inicial():
 
