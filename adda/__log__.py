63be75c4de2aba45fc576a224cb642ed1229f659
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 18:01:26 -0300

    Saved adda/__log__.py @2021-07-05 21:01

--- adda/__log__.py
+++ adda/__log__.py
@@ -13,3 +13,21 @@ SyntaxError: invalid syntax
                                                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jul 05 2021 18:01:25.572 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 44
+    inicial().vai()
+  module <module> line 25
+    self.personagem = Elemento(img = FOLHA, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
+NameError: name 'FOLHA' is not defined
+'''},
