b6bb6eb0994972462a9a0df752a7ff92c5c52a1a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 18:05:13 -0300

    Saved adda/main.py @2021-07-05 21:05

--- adda/main.py
+++ adda/main.py
@@ -20,11 +20,11 @@ class inicial():
         self.fundo = Cena(FUNDO)
         self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
         document.bind("keydown", self.andapersonagem)
-    def folha(self):    
+    #def folha(self):    
         self.x2 = 50
         self.y2 = 470
         self.fundo = Cena(FUNDO)
-        self.folha = Elemento(img = FOLHA, cena = self.fundo, x=self.x2, y=self.y2, h=80, w=80)
+        self.folha = Elemento(img = FOLHA, cena = self.fundo, x2=self.x2, y2=self.y2, h=80, w=80)
         
         
     def vai(self):
