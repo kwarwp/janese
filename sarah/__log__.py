5b3f7dd604fa640c4f83b345b5ebe499a8ed198c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 17:17:03 -0300

    Saved sarah/__log__.py @2021-07-05 20:17

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -76,3 +76,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Jul 05 2021 17:17:02.973 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  self.fundo.vai()
+  ^
+IndentationError: expected an indented block
+'''},
