727a6e94f0d5dc1814b679c6da46ac83839d2a58
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 17:32:13 -0300

    Saved roxanne/main.py @2021-07-05 20:32

--- roxanne/main.py
+++ roxanne/main.py
@@ -8,7 +8,7 @@ STYLE["width"]=500
 STYLE["height"]=120
 
 class inicial():
-    def init(self):
+    def __init__(self):
         self.x1 = 0
         self.y1 = 500
         self.fundo = Cena(FUNDO)
@@ -31,5 +31,5 @@ class inicial():
     
         
         
-if name == "main":
+if __name__ == "__main__":
     inicial().vai()
