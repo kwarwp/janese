596e9b5be506c8f48f34c8ea2a891af516ec1883
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 21:41:19 -0300

    Saved anastasia/main.py @2021-07-06 00:41

--- anastasia/main.py
+++ anastasia/main.py
@@ -4,7 +4,8 @@ from _spy.vitollino.main import Cena, Elemento, STYLE
 from browser import document
 
 FUNDO = "https://imgur.com/bg1h3oD.png"
-PERSONAGEM = "https://imgur.com/7qQzMJb.png"
+PERSONAGEM = "https://imgur.com/1b13SPC"
+PERSONAGEM_ANDANDO = "https://imgur.com/KCLilyv"
 
 STYLE["width"]=1150
 STYLE["height"]=550
