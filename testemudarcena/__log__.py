26a786bf561b809eeb6dc4dd6fdd744d2b9664f4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-11 13:58:44 -0300

    Saved testemudarcena/__log__.py @2021-07-11 16:58

--- testemudarcena/__log__.py
+++ testemudarcena/__log__.py
@@ -59,3 +59,23 @@ NameError: name 'MARCADOR_X' is not defined
     self.marcadorx = Elemento(MARCADOR_X, h=80 , w=80, x=280, y=450) #cria um elemento posicionado 'acima' no joystick
 NameError: name 'MARCADOR_X' is not defined
 '''},
+{'date': 'Sun Jul 11 2021 13:58:41.16 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 84
+    teste()
+  module <module> line 23
+    bonequinha = Persona_control( fundo) 
+  module <module> line 46
+    self.marcadorx = Elemento(MARCADOR_X, h=80 , w=80, x=280, y=450) #cria um elemento posicionado 'acima' no joystick
+NameError: name 'MARCADOR_X' is not defined
+'''},
