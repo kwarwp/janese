c5de3187f45aebd9a73d800c81bf1113b1f72a06
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-11 13:54:40 -0300

    Created testemudarcena/__log__.py @2021-07-11 16:54

---
+++ testemudarcena/__log__.py
@@ -0,0 +1,21 @@
+
+{'date': 'Sun Jul 11 2021 13:54:35.568 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 84
+    teste()
+  module <module> line 23
+    bonequinha = Persona_control( fundo) 
+  module <module> line 46
+    self.cima = Elemento(MARCADOR_X, h=40 , w=40, x=780, y=450, vai=self.anda_cima) #cria um elemento posicionado 'acima' no joystick
+NameError: name 'MARCADOR_X' is not defined
+'''},
