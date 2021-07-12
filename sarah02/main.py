9d3330902f82dfb1bf4d0582209a36f9226dd980
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-12 17:27:35 -0300

    Created sarah02/main.py @2021-07-12 20:27

---
+++ sarah02/main.py
@@ -0,0 +1,36 @@
+# janese.sarah02.main.py
+from_spy.vitollino.main import Cena, Elemento, STYLE
+from brownser import document
+
+FUNDO = "https://imgur.com/DPM4HbC.png"
+PERSONAGEM = "https://imgur.com/azAG8JJ.png"
+
+STYLE["wedth"]= 500
+STYLE["height"]= 200
+
+class inicial():
+    def __init__(self):
+        self.x1 = 0
+        self.y1 = 500
+        self.fundo = Cena(FUNDO)
+        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=50, w=50)
+        document.bind("keydown", self.andapersonagem)
+
+    def vai(self):
+        self.fundo.vai()
+
+    def andapersonagem(self, ev=None):
+        teclado = ev.keyCode
+
+        if teclado in [37, 39]:
+            teclado = (teclado - 38) * 20 #podem modificar o valor do 10
+            self.personagem.x = self.personagem.x + teclado
+
+        elif teclado in [38, 40]:
+            teclado = (teclado - 39) * 10
+            self.personagem.y = self.personagem.y + teclado
+
+
+
+if __name__ == "__main__":
+    inicial().vai()
