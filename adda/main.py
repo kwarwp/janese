6e2d9e7020f09f9da7c4c6cbf6587d3a00de8b6b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-12 18:03:43 -0300

    Saved adda/main.py @2021-07-12 21:03

--- adda/main.py
+++ adda/main.py
@@ -19,7 +19,6 @@ class inicial():
         self.x = 0
         self.y = 460
         self.fundo = Cena(FUNDO)
-        self.folha()
         self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
         document.bind("keydown", self.andapersonagem)
         self.pena = Elemento(img = PENA, cena = self.fundo, x= 200, y=400, h= 80, w=80)
