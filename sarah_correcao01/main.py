b94ac23521828df8bb4fd61bdb08652a9f45eb39
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-12 17:53:55 -0300

    Saved sarah_correcao01/main.py @2021-07-12 20:53

--- sarah_correcao01/main.py
+++ sarah_correcao01/main.py
@@ -8,8 +8,8 @@ from browser import document
 FUNDO = "https://imgur.com/DPM4HbC.png"
 PERSONAGEM = "https://imgur.com/azAG8JJ.png"
 
-STYLE["wedth"]=500
-STYLE["height"]= 200
+STYLE["width"]=1500
+STYLE["height"]= 2000
 
 class inicial():
     def __init__(self):
