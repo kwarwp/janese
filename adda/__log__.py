bbe3d929b9274ca55f9407bdb8f34310162087bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-12 18:03:34 -0300

    Saved adda/__log__.py @2021-07-12 21:03

--- adda/__log__.py
+++ adda/__log__.py
@@ -63,3 +63,21 @@ IndentationError: unexpected indent
     self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
 AttributeError: 'inicial' object has no attribute 'x1'
 '''},
+{'date': 'Mon Jul 12 2021 18:03:34.672 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 44
+    inicial().vai()
+  module <module> line 22
+    self.folha()
+AttributeError: 'inicial' object has no attribute 'folha'
+'''},
