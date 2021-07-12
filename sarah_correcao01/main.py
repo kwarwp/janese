c94c5b132be5c2450f7906139ba681bc6b1e3831
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-12 17:51:22 -0300

    Saved sarah_correcao01/main.py @2021-07-12 20:51

--- sarah_correcao01/main.py
+++ sarah_correcao01/main.py
@@ -16,7 +16,7 @@ class inicial():
         self.x1 = 0
         self.y1 = 500
         self.fundo = Cena(FUNDO)
-        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=150, w=150)
+        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
         document.bind("keydown", self.andapersonagem)
 
     def vai(self):
