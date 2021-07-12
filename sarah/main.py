9bd20fb7e4f7a64bb76f25d9229ca273894f49db
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-12 17:58:38 -0300

    Saved sarah/main.py @2021-07-12 20:58

--- sarah/main.py
+++ sarah/main.py
@@ -8,7 +8,7 @@ FUNDO = "https://imgur.com/DPM4HbC.png"
 PERSONAGEM = "https://imgur.com/azAG8JJ.png"
 
 STYLE["width"]= 1000
-STYLE["height"]= 400
+STYLE["height"]= 500
 
 class inicial():
     def __init__(self):
