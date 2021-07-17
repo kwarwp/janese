859e43f0acd524d0f9ff8a2f809befba9741fc29
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-17 18:42:47 -0300

    Saved heather/__log__.py @2021-07-17 21:42

--- heather/__log__.py
+++ heather/__log__.py
@@ -85,3 +85,21 @@ NameError: name 'name' is not defined
     self.fundo.vai()
 AttributeError: 'inicial' object has no attribute 'fundo'
 '''},
+{'date': 'Sat Jul 17 2021 18:42:47.484 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    inicial().vai()
+  module <module> line 22
+    self.fundo.vai()
+AttributeError: 'inicial' object has no attribute 'fundo'
+'''},
