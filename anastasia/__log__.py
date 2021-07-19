4186998a50de15ef2f4bfc92789a3cfeca0a1adc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-19 17:13:47 -0300

    Saved anastasia/__log__.py @2021-07-19 20:13

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -156,3 +156,21 @@ AttributeError: 'module' object has no attribute '__setitem__'
     self.fundo = Cena(FUNDO)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Mon Jul 19 2021 17:13:46.766 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 79
+    teste()
+  module <module> line 23
+    bonequinha = Persona_control(fundo) 
+TypeError: __init__() takes 1 positional argument but more were given
+'''},
