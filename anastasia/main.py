9129ad54df9923b9abe6116849e1a79ab5f30e70
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-19 17:20:07 -0300

    Saved anastasia/main.py @2021-07-19 20:20

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,11 +1,11 @@
 #Profa Gi :)
 
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-from browser import document
+
 
 FUNDO = "https://imgur.com/bg1h3oD.png"
 PERSONAGEM = "https://imgur.com/0gdXZn8.gif"
-PERSONAGEM_ANDANDO = "https://imgur.com/KCLilyv.png"
+
 JOYSTICK_FALSO = "https://imgur.com/KiYDtv2.png"
 MARCADOR_X = "https://imgur.com/kJ2MkuK.png"
 MARCADOR_ESQUERDA = "https://imgur.com/hEF8XPG.png"
@@ -17,19 +17,31 @@ MARCADOR_BAIXO = "https://imgur.com/hEF8XPG.png"
 STYLE["width"]=1150
 STYLE["height"]=550
 
+
 def teste():
     fundo = Cena(FUNDO) 
     fundo.vai()
-    bonequinha = Persona_control(fundo) 
-
-
-class Persona_control():
-
-    def __init__(self):
-        self.x = 1000
-        self.y = 400
-                
-        self.joystickfalso = Elemento(JOYSTICK_FALSO, h=100 , w=100, x=750, y=440) #cria um elemento posicionado 'acima' no joystick
+    bonequinha = Persona_control( fundo) 
+        
+class Persona_control:
+    """ Cria um elemento que anda a partir do clique no joystick
+    
+        self.nome_do_elemento = Persona_control( nome_do_fundo)
+        
+        :param nome_do_fundo: Insere o personagem em um fundo pré-criado
+        
+    """
+    def __init__(self, nome_do_fundo):
+        self.x = 10 # valor pré-estabelecido do x
+        self.y = 430 # valor pré-estabelecido do y
+        
+        self.persona = Elemento(PERSONAGEM, h=100 , w=100, x=self.x, y=self.y) # cria Elemento 
+        self.persona.entra(nome_do_fundo) # utiliza o método entra() da classe Elemento para não ter que criar um atributo cena para a classe persona_control 
+        
+        #self.persona.x = self.x
+        #self.persona.y = self.y
+        
+        self.joystickfalso = Elemento(JOYSTICK_FALSO, h=150 , w=150, x=720, y=420) #cria um elemento posicionado 'acima' no joystick
         self.joystickfalso.entra(nome_do_fundo)
         
         self.marcadorx = Elemento(MARCADOR_X, h=70 , w=70, x=80, y=450) #cria um elemento posicionado 'acima' no joystick
@@ -38,7 +50,7 @@ class Persona_control():
         self.cima = Elemento(MARCADOR_CIMA, h=40 , w=40, x=780, y=450, vai=self.anda_cima) #cria um elemento posicionado 'acima' no joystick
         self.cima.entra(nome_do_fundo)
         
-        self.baixo = Elemento(MARCADOR_BAIXO, h=40 , w=40, x=780, y=500, vai=self.anda_baixo) #cria um elemento posicionado 'abaixo' no joystick
+        self.baixo = Elemento(MARCADOR_BAIXO, h=40 , w=40, x=780, y=530, vai=self.anda_baixo) #cria um elemento posicionado 'abaixo' no joystick
         self.baixo.entra(nome_do_fundo)
         
         self.direita = Elemento(MARCADOR_DIREITA, h=40 , w=40, x=720, y=490,vai=self.anda_direita) #cria um elemento posicionado 'à direita' no joystick
@@ -46,13 +58,7 @@ class Persona_control():
         
         self.esquerda = Elemento(MARCADOR_ESQUERDA, h=40 , w=40, x=840, y=490, vai=self.anda_esquerda) #cria um elemento posicionado 'à esquerda' no joystick
         self.esquerda.entra(nome_do_fundo)
-        
-        self.persona = Elemento(PERSONAGEM, h=170 , w=190, x=self.x, y=self.y) # cria Elemento 
-        self.persona.entra(nome_do_fundo) # utiliza o método entra() da classe Elemento para não ter que criar um atributo cena para a classe persona_control
 
-
-        
-        
     def anda_direita(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'direita' é clicado.
         """
