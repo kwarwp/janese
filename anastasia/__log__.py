99e34411acf526097f12823ff764d8ad9ad75527
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-26 16:58:31 -0300

    Saved anastasia/__log__.py @2021-07-26 19:58

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -210,3 +210,19 @@ TypeError: __init__() takes 1 positional argument but more were given
     bonequinha = Persona_control(fundo) 
 TypeError: __init__() takes 1 positional argument but more were given
 '''},
+{'date': 'Mon Jul 26 2021 16:58:30.27 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    inicial.vai()
+TypeError: vai() missing 1 positional argument: self
+'''},
