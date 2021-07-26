a1227340a8bda80269f0d0c1dee7b8687eaf04cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-26 17:52:49 -0300

    Saved adda/main.py @2021-07-26 20:52

--- adda/main.py
+++ adda/main.py
@@ -9,35 +9,83 @@ FUNDO = "https://i.imgur.com/KYKStgT.png"
 PERSONAGEM = "https://imgur.com/E6lSl7l.gif"
 FOLHA = "https://i.imgur.com/upAN1GX.png"
 PENA = "https://i.imgur.com/916QFLA.png"
+MARCADOR_X = "https://imgur.com/kJ2MkuK.png"
+MARCADOR_ESQUERDA = "https://imgur.com/GTnLEnS.png"
+MARCADOR_DIREITA = "https://imgur.com/Nl7Qy9h.png"
+MARCADOR_CIMA = "https://imgur.com/f2zfcvx.png"
+MARCADOR_BAIXO = "https://imgur.com/QDx4zgx.png"
+
 
 STYLE["width"]=1350
 STYLE["height"]=720
 
-class inicial():
+def teste():
+    fundo = Cena(FUNDO) 
+    fundo.vai()
+    bonequinha = Persona_control( fundo) 
+        
+class Persona_control:
+    """ Cria um elemento que anda a partir do clique no joystick
+    
+        self.nome_do_elemento = Persona_control( nome_do_fundo)
+        
+        :param nome_do_fundo: Insere o personagem em um fundo pré-criado
+        
+    """
+    def __init__(self, nome_do_fundo):
+        self.x = 10 # valor pré-estabelecido do x
+        self.y = 430 # valor pré-estabelecido do y
+        
+        #self.persona.x = self.x
+        #self.persona.y = self.y
+        
+        """self.joystickfalso = Elemento(JOYSTICK_FALSO, h=100 , w=100, x=750, y=440) #cria um elemento posicionado 'acima' no joystick
+        self.joystickfalso.entra(nome_do_fundo)"""
+        
+        self.marcadorx = Elemento(MARCADOR_X, h=70 , w=70, x=80, y=450) #cria um elemento posicionado 'acima' no joystick
+        self.marcadorx.entra(nome_do_fundo)
+        
+        self.cima = Elemento(MARCADOR_CIMA, h=40 , w=40, x=780, y=450, vai=self.anda_cima) #cria um elemento posicionado 'acima' no joystick
+        self.cima.entra(nome_do_fundo)
+        
+        self.baixo = Elemento(MARCADOR_BAIXO, h=40 , w=40, x=780, y=500, vai=self.anda_baixo) #cria um elemento posicionado 'abaixo' no joystick
+        self.baixo.entra(nome_do_fundo)
+        
+        self.direita = Elemento(MARCADOR_DIREITA, h=40 , w=40, x=720, y=490,vai=self.anda_direita) #cria um elemento posicionado 'à direita' no joystick
+        self.direita.entra(nome_do_fundo)
+        
+        self.esquerda = Elemento(MARCADOR_ESQUERDA, h=40 , w=40, x=840, y=490, vai=self.anda_esquerda) #cria um elemento posicionado 'à esquerda' no joystick
+        self.esquerda.entra(nome_do_fundo)
+        
+        self.persona = Elemento(PERSONAGEM, h=170 , w=190, x=self.x, y=self.y) # cria Elemento 
+        self.persona.entra(nome_do_fundo) # utiliza o método entra() da classe Elemento para não ter que criar um atributo cena para a classe persona_control 
+        
 
-    def __init__(self):
-        self.x1 = 0
-        self.y1 = 440
-        self.fundo = Cena(FUNDO)
-        self.personagem = Elemento(img = PERSONAGEM, cena = self.fundo, x=self.x1, y=self.y1, h=100, w=100)
-        document.bind("keydown", self.andapersonagem)
-        self.pena = Elemento(img = PENA, cena = self.fundo, x= 200, y=420, h= 80, w=80)
+
+
+    def anda_direita(self,*_):
+        """Este método guarda a expressão de movimentação do elemento quando o botão 'direita' é clicado.
+        """
+        self.persona.x = self.x = self.x - 10
+        #self.persona.x = self.x - 10 > Deixar para averiguações posteriores
+        #self.persona.x = self.x -= 10 > Deixar para averiguações posteriores
         
+    def anda_esquerda(self,*_):
+        """Este método guarda a expressão de movimentação do elemento quando o botão 'esquerda' é clicado.
+        """
+        self.persona.x = self.x = self.x + 10
         
-    def vai(self):
-        self.fundo.vai()
+    def anda_cima(self,*_):
+        """Este método guarda a expressão de movimentação do elemento quando o botão 'em cima' é clicado.
+        """
+        self.persona.y = self.y = self.y - 10
         
-    def andapersonagem(self, ev=None):
-        teclado = ev.keyCode
-        
-        if teclado in [37,39]:
-            teclado = (teclado - 38) * 10
-            self.personagem.x = self.personagem.x + teclado
-            
-        elif teclado in [38, 40]:
-            teclado = (teclado - 39) * 10
-            self.personagem.y = self.personagem.y + teclado
-        
-        
-if __name__ == "__main__":
-    inicial().vai()
+    def anda_baixo(self,*_):
+        """Este método guarda a expressão de movimentação do elemento quando o botão 'embaixo' é clicado.
+        """
+        self.persona.y = self.y = self.y + 10
+
+if __name__ == "__main__":  
+    teste()
+
+
