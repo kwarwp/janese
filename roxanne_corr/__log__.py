4368a16986de9dc294a8ecef789285357287ab69
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-02 17:56:25 -0300

    Saved roxanne_corr/__log__.py @2021-08-02 20:56

--- roxanne_corr/__log__.py
+++ roxanne_corr/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 02 2021 17:56:25.505 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 71
+  if self.persona.x = self.x = self.x - 10
+                     ^
+SyntaxError: invalid syntax
+'''},
