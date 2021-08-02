101b588c39a5769a1a4fe0fb883551c2e2501d09
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-02 17:57:57 -0300

    Saved testemudarcena/__log__.py @2021-08-02 20:57

--- testemudarcena/__log__.py
+++ testemudarcena/__log__.py
@@ -93,3 +93,21 @@ SyntaxError: invalid syntax
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 02 2021 17:57:55.929 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 120
+    Eventos()
+  module <module> line 44
+    self.ambiente = Cena(self.CENA_corredor_1)
+AttributeError: 'Eventos' object has no attribute 'CENA_corredor_1'
+'''},
