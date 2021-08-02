a1d49257bd224623d734afdf15b15e8ea0001549
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-02 17:41:02 -0300

    Saved anastasia/__log__.py @2021-08-02 20:41

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -269,3 +269,25 @@ SyntaxError: invalid syntax
     fundo = Cena(FUNDO, dentro[elemento1, elemento2]) 
 NameError: name 'dentro' is not defined
 '''},
+{'date': 'Mon Aug 02 2021 17:41:01.176 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 97
+    teste()
+  module <module> line 29
+    fundo = Cena(FUNDO, dentro=[elemento1, elemento2]) 
+  module _spy.vitollino.main line 1095
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 1138
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+TypeError: list indices must be integer, not str
+'''},
