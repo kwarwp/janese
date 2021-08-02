8cd796d204bd81e25bb1969a07f8394d06200d34
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-02 17:55:27 -0300

    Created roxanne_corr/__log__.py @2021-08-02 20:55

---
+++ roxanne_corr/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 02 2021 17:55:24.778 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 71
+  if anda_direita & anda_direita
+                                 ^
+SyntaxError: invalid syntax
+'''},
