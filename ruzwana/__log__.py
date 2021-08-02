865f699f1868ee768ebde058665396c306596de4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-02 17:41:46 -0300

    Saved ruzwana/__log__.py @2021-08-02 20:41

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -15,3 +15,23 @@
     if name == "main":
 NameError: name 'name' is not defined
 '''},
+{'date': 'Mon Aug 02 2021 17:41:50.847 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    teste()
+  module <module> line 16
+    bonequinha = Persona_control( fundo) 
+  module <module> line 36
+    self.marcadorx = Elemento(MARCADOR_X, h=70 , w=70, x=80, y=450) #cria um elemento posicionado 'acima' no joystick
+NameError: name 'MARCADOR_X' is not defined
+'''},
