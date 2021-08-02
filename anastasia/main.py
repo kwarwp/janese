c62e00395581c3e12bce9927c05aaa394238ddb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-02 17:44:27 -0300

    Saved anastasia/main.py @2021-08-02 20:44

--- anastasia/main.py
+++ anastasia/main.py
@@ -26,8 +26,8 @@ STYLE["height"]=550
 def teste():
     elemento1 = Elemento(Colecionavel, h=50 , w=50, x=100, y=430) 
     elemento2 = Elemento(Colecionavel, h=50 , w=50, x=150, y=430) 
-    lista = [elemento1, elemento2]
-    fundo = Cena(FUNDO, dentro=lista) 
+    fundo = Cena(FUNDO) 
+    fundo.bota(elemento1)
     fundo.vai()
     bonequinha = Persona_control(fundo) 
     
