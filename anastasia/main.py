9a82b1db1fcbb357f3199d75eed69d13e01dd3cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-03 15:42:48 -0300

    Saved anastasia/main.py @2021-08-03 18:42

--- anastasia/main.py
+++ anastasia/main.py
@@ -95,7 +95,7 @@ def cena1():
     bonequinha = Persona_control(c11)           
     pega = Elemento(MARCADOR_MEIO, h=40 , w=40, x=1005, y=470, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = c11,
-                           vai = pega)
+                           vai = cena2)
     proxima = Elemento(MARCADOR_ESQUERDA, tit="Próxima Cena",
                            h=30 , w=30, x=1100, y=220, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = c11,
