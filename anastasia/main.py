2118d6cd59deb65dcf4c09d0fe95abba27fc812d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-02 23:04:33 -0300

    Saved anastasia/main.py @2021-08-03 02:04

--- anastasia/main.py
+++ anastasia/main.py
@@ -121,7 +121,8 @@ class Persona_control:
     def anda_direita(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'direita' é clicado.
         """
-        self.persona.x = self.x = self.x - 20
+        if (self.x != 0):
+            self.persona.x = self.x = self.x - 20
         #self.persona.x = self.x - 10 > Deixar para averiguações posteriores
         #self.persona.x = self.x -= 10 > Deixar para averiguações posteriores
         
