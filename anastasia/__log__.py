66d8e58aeabf23b4b1ba40e02bb0f4efb7df6813
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-03 16:01:58 -0300

    Saved anastasia/__log__.py @2021-08-03 19:01

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -675,3 +675,21 @@ UnboundLocalError: local variable 'pega' referenced before assignment
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Aug 03 2021 16:01:57.442 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 172
+    cena1()
+  module <module> line 105
+    nome_da_musica = Musica(MUSICA, loop = True, autoplay = True)
+NameError: name 'Musica' is not defined
+'''},
