8b1dc1877ece3302414a3d48fe4d55901e142d4b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 17:52:54 -0300

    Saved adda/main.py @2021-08-16 20:52

--- adda/main.py
+++ adda/main.py
@@ -94,7 +94,7 @@ class Persona_control:
     def anda_esquerda(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'esquerda' é clicado.
         """
-        self.persona1.x = self.x = self.x + 20
+        self.persona.x = self.x = self.x + 20
         
     def anda_cima(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'em cima' é clicado.
