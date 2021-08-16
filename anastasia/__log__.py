1dbfba711cbf26d4bfb0a6c641eee9334a8c20d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 17:58:20 -0300

    Saved anastasia/__log__.py @2021-08-16 20:58

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -837,3 +837,23 @@ AttributeError: 'GUI' object has no attribute 'coloca_no_inventario'
     item = Elemento(ITEM, tit="Item", h=30 , w=30, x=400, y=450, cena = c11,
 AttributeError: 'GUI' object has no attribute 'coloca_no_inventario'
 '''},
+{'date': 'Mon Aug 16 2021 17:58:19.320 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 211
+    cena1()
+  module <module> line 127
+    bonequinha = Persona_control(c11)
+  module <module> line 161
+    self.persona1.siz(0,0)       
+TypeError: 'list' object is not callable
+'''},
