fb0637b7e41abb7bc2a96391bc116fd723c64470
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 17:17:07 -0300

    Saved anastasia/__log__.py @2021-08-16 20:17

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -729,3 +729,21 @@ NameError: name 'inv' is not defined
     inv.inicia() 
 NameError: name 'inv' is not defined
 '''},
+{'date': 'Mon Aug 16 2021 17:17:06.995 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 210
+    cena1()
+  module <module> line 117
+    inv.inicia() 
+NameError: name 'inv' is not defined
+'''},
