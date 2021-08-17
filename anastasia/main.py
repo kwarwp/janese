ece39c99f139fabc9e65a98c4d600150806ef0cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-17 17:38:52 -0300

    Saved anastasia/main.py @2021-08-17 20:38

--- anastasia/main.py
+++ anastasia/main.py
@@ -160,7 +160,7 @@ class Jogo:
         pega = Elemento(MARCADOR_MEIO, tit = "Pegar", h=40 , w=40, x=1005, y=470, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = self.c11)
                            
-if self.olha==item1:                   
+if itens.append(item1):                 
         pega.elt.bind("click", self.pega_acao)
     #Inserindo o botão que muda de cena
         bateria = Elemento(BATERIA, tit="Bateria",
