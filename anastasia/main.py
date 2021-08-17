b86a57e41c04969ee716a24e122e07a6389aeb56
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 21:59:12 -0300

    Saved anastasia/main.py @2021-08-17 00:59

--- anastasia/main.py
+++ anastasia/main.py
@@ -79,9 +79,7 @@ essa lista existe, e vai ser possível acessar ela em qualquer função abaixo.
 cenas = [c11, c12, c13, c14, c21, c22, c23]
 ind_cenas = 0
 itens = []
-score = 0
-x_personagem = 0
-y_personagem = 0
+self.score = 0
 
 
 def pega_acao(event = None):
@@ -89,7 +87,7 @@ def pega_acao(event = None):
     for i in itens:
         i.h = 0
         i.w = 0
-        score = score + 1
+        self.score = score + 1
         carga = Elemento(CARGA, w = 50, h = 50, x=1050, y=20)
         cenas[ind_cenas].bota()
             
