6a0f2dd426fa5c21bbfb31a0433af7fa345934a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-17 15:30:31 -0300

    Saved anastasia/main.py @2021-08-17 18:30

--- anastasia/main.py
+++ anastasia/main.py
@@ -2,6 +2,37 @@
 
 #Profa Gi :)
 
+'''Uma forma de fazer a passagem de cenas é criando salas - que permitem 4 cenas
+e depois criando o labirinto, adicionando as salas nele.
+
+# Cria Salas
+# n = norte, s = sul, l = leste, o = oeste
+# Mas serve só para se localizar, dá pra usar de outras formas
+# Acho que dá pra fazer com lista também, mas to seguindo o que tava naquela documentação
+
+s1 = Sala(n = c11, s = c12, l = c13, o = c14)
+s2 = Sala(n = c21, s = c22, l = c23)
+
+# Cria o labirinto de Salas
+# c = centro, n = norte
+la = Labirinto(c=s1,n=s2)
+
+
+Essa é a forma descrita na documentação da biblioteca que usamos para criar o jogo, ela funciona, 
+porém como cada sala espera 4 cenas, e o labirinto várias salas, quando alguma delas fica vazia, 
+pode acontecer de um clique na tela direcionar para uma sala nula = que não existe. 
+Para arrumar isso seria necessário criar uma ação que não faz nada, e 
+na posição da sala que não existe adicionar uma sala que 'não faz nada'
+'''
+
+'''
+Pelo o que entendi da ideia de jogo de vocês, as cenas seriam todas sequenciais, então esse esquema de 
+salas e labirinto seria um pouco confuso de implementar.
+Por isso, outra maneira seria criando uma lista de cenas.
+Essa lista tem que ser criada aqui em cima no código, pq ela vai ser uma variável global, ou seja,
+essa lista existe, e vai ser possível acessar ela em qualquer função abaixo.
+'''
+
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto, Sala, Labirinto
 
 
@@ -34,64 +65,35 @@ CARGA = "https://imgur.com/dipQTCT.png"
 STYLE["width"]=1150
 STYLE["height"]=500
 
-#Cria Cenas
-c11 = Cena(FUNDO_CENA1)
-c12 = Cena(FUNDO_CENA2)
-c13 = Cena(FUNDO_CENA3)
-c14 = Cena(FUNDO_CENA4)
+class Jogo:
 
-c21 = Cena(FUNDO_CENA5)
-c22 = Cena(FUNDO_CENA6)
-c23 = Cena(FUNDO_CENA7)
+    
+    def __init__(self):
+        #Cria Cenas
+        self.c11 = Cena(FUNDO_CENA1)
+        self.c12 = Cena(FUNDO_CENA2)
+        self.c13 = Cena(FUNDO_CENA3)
+        self.c14 = Cena(FUNDO_CENA4)
 
-'''Uma forma de fazer a passagem de cenas é criando salas - que permitem 4 cenas
-e depois criando o labirinto, adicionando as salas nele.
-
-# Cria Salas
-# n = norte, s = sul, l = leste, o = oeste
-# Mas serve só para se localizar, dá pra usar de outras formas
-# Acho que dá pra fazer com lista também, mas to seguindo o que tava naquela documentação
-
-s1 = Sala(n = c11, s = c12, l = c13, o = c14)
-s2 = Sala(n = c21, s = c22, l = c23)
-
-# Cria o labirinto de Salas
-# c = centro, n = norte
-la = Labirinto(c=s1,n=s2)
-
-
-Essa é a forma descrita na documentação da biblioteca que usamos para criar o jogo, ela funciona, 
-porém como cada sala espera 4 cenas, e o labirinto várias salas, quando alguma delas fica vazia, 
-pode acontecer de um clique na tela direcionar para uma sala nula = que não existe. 
-Para arrumar isso seria necessário criar uma ação que não faz nada, e 
-na posição da sala que não existe adicionar uma sala que 'não faz nada'
-'''
-
-'''
-Pelo o que entendi da ideia de jogo de vocês, as cenas seriam todas sequenciais, então esse esquema de 
-salas e labirinto seria um pouco confuso de implementar.
-Por isso, outra maneira seria criando uma lista de cenas.
-Essa lista tem que ser criada aqui em cima no código, pq ela vai ser uma variável global, ou seja,
-essa lista existe, e vai ser possível acessar ela em qualquer função abaixo.
-'''
-
-
-cenas = [c11, c12, c13, c14, c21, c22, c23]
-itens = []
-score = []
-ind_cenas= [0]
-
-
-
-def pega_acao(event = None):
-    print(self.bonequinha.get_x)
-    for i in itens:
+        self.c21 = Cena(FUNDO_CENA5)
+        self.c22 = Cena(FUNDO_CENA6)
+        self.c23 = Cena(FUNDO_CENA7)
         
-        i.h = 0
-        i.w = 0
-        score.append(1)
-        carga = Elemento(CARGA, w = 50, h = 50, x=1050, y=20)
-        cenas[ind_cenas[0]].bota(carga)
+        self.cenas = [self.c11, self.c12, self.c13, self.c14, self.c21, self.c22, self.c23]
+        self.itens = []
+        self.score = 0
+        self.ind_cenas= 0
+
+
+    def pega_acao(event = None):
+        print(self.bonequinha.get_x)
+        for i in self.itens:
+        
+            i.h = 0
+            i.w = 0
+            self.score = self.score + 1
+            carga = Elemento(CARGA, w = 50, h = 50, x=1050, y=20)
+            self.cenas[self.ind_cenas].bota(carga)
             
     
 
@@ -99,64 +101,64 @@ def pega_acao(event = None):
     
 
 #Função para ir para a cena 3    
-def cena3(event = None):
+    def cena3(event = None):
     #la.centro.leste.vai()
-    cenas[2].vai()
-    texto_ = Texto(c13, txt = "Terceira Cena")
-    texto_.vai()
-    bonequinha = Persona_control(c13)
-    botao = Elemento(MARCADOR_ESQUERDA, tit="Próxima Cena",
+        self.cenas[2].vai()
+        texto_ = Texto(self.c13, txt = "Terceira Cena")
+        texto_.vai()
+        bonequinha = Persona_control(self.c13)
+        botao = Elemento(MARCADOR_ESQUERDA, tit="Próxima Cena",
                            h=30 , w=30, x=1100, y=220, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
-                           cena = c13,
+                           cena = self.c13,
                            vai = cena4)
                            
 #Função para ir para a cena 2
-def cena2(event = None):
+    def cena2(event = None):
     #la.centro.sul.vai()
-    cenas[1].vai()
-    texto_ = Texto(c12, txt = "Segunda Cena")
-    texto_.vai()
-    bonequinha = Persona_control(c12)
-    botao = Elemento(MARCADOR_ESQUERDA, tit="Próxima Cena",
+        self.cenas[1].vai()
+        texto_ = Texto(self.c12, txt = "Segunda Cena")
+        texto_.vai()
+        bonequinha = Persona_control(self.c12)
+        botao = Elemento(MARCADOR_ESQUERDA, tit="Próxima Cena",
                            h=30 , w=30, x=1100, y=220, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
-                           cena = c12,
+                           cena = self.c12,
                            vai = cena3)
     
 #Função que cria a cena 1
-def cena1():
+    def cena1(self):
     #Código se fosse usar o sistema de labirinto
     #la.centro.norte.vai()
     
     
     #Código usando lista
     #Lembrando que lista em python começa sempre da posição 0
-    cenas[0].vai()
+        cenas[0].vai()
     
-    item1 = Elemento(ITEM, tit="Item", h=30 , w=30, x=300, y=500, cena = c11)
-    item2 = Elemento(ITEM, tit="Item", h=30 , w=30, x=400, y=480, cena = c11)
-    item3 = Elemento(ITEM, tit="Item", h=30 , w=30, x=550, y=490, cena = c11)
-    itens.append(item1)
-    itens.append(item2)
-    itens.append(item3)
-    score = 0
+        item1 = Elemento(ITEM, tit="Item", h=30 , w=30, x=300, y=500, cena = self.c11)
+        item2 = Elemento(ITEM, tit="Item", h=30 , w=30, x=400, y=480, cena = self.c11)
+        item3 = Elemento(ITEM, tit="Item", h=30 , w=30, x=550, y=490, cena = self.c11)
+        self.itens.append(item1)
+        self.itens.append(item2)
+        self.itens.append(item3)
+        self.score = 0
 
     
     #Para inserir pop up
-    texto_ = Texto(c11, txt = "Primeira Cena")
-    texto_.vai()
+        texto_ = Texto(self.c11, txt = "Primeira Cena")
+        texto_.vai()
     #Inserindo a boneca
-    self.bonequinha = Persona_control(c11)
+        self.bonequinha = Persona_control(self.c11)
     
     
-    pega = Elemento(MARCADOR_MEIO, tit = "Pegar", h=40 , w=40, x=1005, y=470, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
-                           cena = c11)
-    pega.elt.bind("click", pega_acao)
+        pega = Elemento(MARCADOR_MEIO, tit = "Pegar", h=40 , w=40, x=1005, y=470, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = self.c11)
+        pega.elt.bind("click", pega_acao)
     #Inserindo o botão que muda de cena
-    bateria = Elemento(BATERIA, tit="Bateria",
-                           h=50 , w=50, x=1050, y=20, cena= c11)
-    proxima = Elemento(MARCADOR_ESQUERDA, tit="Próxima Cena",
+        bateria = Elemento(BATERIA, tit="Bateria",
+                           h=50 , w=50, x=1050, y=20, cena= self.c11)
+        proxima = Elemento(MARCADOR_ESQUERDA, tit="Próxima Cena",
                            h=30 , w=30, x=1100, y=220, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
-                           cena = c11,
+                           cena = self.c11,
                            vai = cena2)
 
 
@@ -252,4 +254,5 @@ class Persona_control:
             self.persona1.y = self.y = self.y + 20
 
 if __name__ == "__main__":  
-    cena1()
+    jogo = Jogo()
+    jogo.cena1()
