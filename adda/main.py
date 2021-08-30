12cb18eeb26f946b1e1b96ee5a72f8e972fb2d10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-30 18:08:44 -0300

    Saved adda/main.py @2021-08-30 21:08

--- adda/main.py
+++ adda/main.py
@@ -138,7 +138,7 @@ class Persona_control:
     def anda_cima(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'em cima' é clicado.
         """
-        if (self.persona.y -20 > 100):
+        if (self.persona.y -20 > 80):
             self.persona.y = self.y = self.y - 20
             self.persona1.y = self.y = self.y - 20
         
