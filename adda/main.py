a7e8cc8ff46ff099416b0277ed4dbe377a6b2d9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-30 17:42:54 -0300

    Saved adda/main.py @2021-08-30 20:42

--- adda/main.py
+++ adda/main.py
@@ -6,8 +6,8 @@ from _spy.vitollino.main import Cena, Elemento, STYLE
 from browser import document
 
 FUNDO = "https://i.imgur.com/KYKStgT.png"
-PERSONAGEM = "https://imgur.com/E6lSl7l.gif"
-PERSONAGEM1= "https://imgur.com/A6wdCYS.gif"
+persona = "https://imgur.com/E6lSl7l.gif"
+persona1= "https://imgur.com/A6wdCYS.gif"
 FOLHA = "https://i.imgur.com/upAN1GX.png"
 PENA = "https://i.imgur.com/916QFLA.png"
 MARCADOR_X = "https://imgur.com/kJ2MkuK.png"
@@ -16,17 +16,19 @@ MARCADOR_ESQUERDA = "https://imgur.com/Nl7Qy9h.png"
 MARCADOR_CIMA = "https://imgur.com/f2zfcvx.png"
 MARCADOR_BAIXO = "https://imgur.com/QDx4zgx.png"
 MAECADOR_PLAY = "https://i.imgur.com/qEbkWNJ.png"
-
+#fase um
 FUNDO_CENA1 = "https://i.imgur.com/kJBS3k9.jpg"
 FUNDO_CENA2 = "https://i.imgur.com/92LwWM9.jpg"
 FUNDO_CENA3 = "https://i.imgur.com/j70TEwA.jpg"
 FUNDO_CENA4 = "https://i.imgur.com/ELVLnWr.jpg"
+#fase dois
 FUNDO_CENA5 = "https://i.imgur.com/eQGB5Yi.jpg"
 FUNDO_CENA6 = "https://i.imgur.com/s0cX4Bg.jpg"
 FUNDO_CENA7 = "https://i.imgur.com/uoIYxmz.jpg"
 FUNDO_CENA8 = "https://i.imgur.com/9TrrFXl.jpg"
 FUNDO_CENA9 = "https://i.imgur.com/8wD3w9e.jpg"
 FUNDO_CENA10 = "https://i.imgur.com/ZvBi2gh.jpg"
+#fase 3
 FUNDO_CENA11 = "https://i.imgur.com/EBbuTmf.jpg"
 FUNDO_CENA12 = "https://i.imgur.com/3v5X9Pk.jpg"
 FUNDO_CENA13 = "https://i.imgur.com/NODyC2J.jpg"
@@ -56,6 +58,24 @@ class Persona_control:
         self.x = 10 # valor pré-estabelecido do x
         self.y = 430 # valor pré-estabelecido do y
         
+        self.c11 = Cena(FUNDO_CENA1)
+        self.c12 = Cena(FUNDO_CENA2)
+        self.c13 = Cena(FUNDO_CENA3)
+        self.c14 = Cena(FUNDO_CENA4)
+
+        self.c21 = Cena(FUNDO_CENA5)
+        self.c22 = Cena(FUNDO_CENA6)
+        self.c23 = Cena(FUNDO_CENA7)
+        self.c24 = Cena(FUNDO_CENA8)
+        self.c25 = Cena(FUNDO_CENA9)
+        self.c26 = Cena(FUNDO_CENA10)
+        
+        self.c31 = Cena(FUNDO_CENA11)
+        self.c32 = Cena(FUNDO_CENA12)
+        self.c33 = Cena(FUNDO_CENA13)
+        self.c34 = Cena(FUNDO_CENA14)
+        self.c35 = Cena(FUNDO_CENA15)
+
         #self.persona.x = self.x
         #self.persona.y = self.y
         
@@ -84,28 +104,57 @@ class Persona_control:
 
 
 
+    def get_x(self):
+        return self.persona.x
+        
+    def get_y(self):
+        return self.persona.y
+
     def anda_direita(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'direita' é clicado.
         """
-        self.persona.x = self.x = self.x - 10
+        '''Quando necessário os personagens devem ser trocados - Um será zerado e o outro volta ao tamanho normal
+        porém ambos devem ser atualizados nas posições para andarem sincronizados, isto para todas as funções 
+        que alterem a posição do personagem'''
+        
+        self.persona1.h = 100
+        self.persona1.w = 100
+        
+        self.persona.h = 0
+        self.persona.w = 0
+        
+        if (self.persona.x - 20 > 0):
+            self.persona.x = self.x = self.x - 20
+            self.persona1.x = self.x = self.x - 20
         #self.persona.x = self.x - 10 > Deixar para averiguações posteriores
         #self.persona.x = self.x -= 10 > Deixar para averiguações posteriores
         
     def anda_esquerda(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'esquerda' é clicado.
         """
-        self.persona.x = self.x = self.x + 20
+        self.persona1.h = 0
+        self.persona1.w = 0
+        
+        self.persona.h = 100
+        self.persona.w = 100
+        
+        if (self.persona.x + 20 < 1100):
+            self.persona.x = self.x = self.x + 20
+            self.persona1.x = self.x = self.x + 20
         
     def anda_cima(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'em cima' é clicado.
         """
-        self.persona.y = self.y = self.y - 20
+        if (self.persona.y - 20 > 400):
+            self.persona.y = self.y = self.y - 20
+            self.persona1.y = self.y = self.y - 20
         
     def anda_baixo(self,*_):
         """Este método guarda a expressão de movimentação do elemento quando o botão 'embaixo' é clicado.
         """
-        self.persona.y = self.y = self.y + 20
-
+        if (self.persona.y + 20 < 500):
+            self.persona.y = self.y = self.y + 20
+            self.persona1.y = self.y = self.y + 20
 if __name__ == "__main__":  
     teste()
 
