3d8f600b921513f1905c7b9c3a23a985b643a8d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-30 17:43:14 -0300

    Saved adda/__log__.py @2021-08-30 20:43

--- adda/__log__.py
+++ adda/__log__.py
@@ -154,3 +154,23 @@ IndentationError: unexpected indent
     self.persona = Elemento(PERSONAGEM, h=170 , w=190, x=self.x, y=self.y) # cria Elemento 
 NameError: name 'PERSONAGEM' is not defined
 '''},
+{'date': 'Mon Aug 30 2021 17:43:14.607 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 159
+    teste()
+  module <module> line 47
+    bonequinha = Persona_control( fundo) 
+  module <module> line 100
+    self.persona = Elemento(PERSONAGEM, h=170 , w=190, x=self.x, y=self.y) # cria Elemento 
+NameError: name 'PERSONAGEM' is not defined
+'''},
