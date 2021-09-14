6afab7c58d5dbb27c71db25cd0ed0435b92bd516
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-14 20:10:50 -0300

    Saved adda/__log__.py @2021-09-14 23:10

--- adda/__log__.py
+++ adda/__log__.py
@@ -208,3 +208,23 @@ SyntaxError: invalid syntax
                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 14 2021 20:10:48.195 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 147
+    teste()
+  module <module> line 42
+    bonequinha = Persona_control( fundo) 
+  module <module> line 58
+    self.c13 = Cena(FUNDO_CENA3)
+NameError: name 'FUNDO_CENA3' is not defined
+'''},
