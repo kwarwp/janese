88d686b521cec89bc4dfa32483f07af4516591c9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-20 17:23:39 -0300

    Saved adda/__log__.py @2021-09-20 20:23

--- adda/__log__.py
+++ adda/__log__.py
@@ -313,3 +313,19 @@ IndentationError: unexpected indent
     jogo.cena1()
 AttributeError: 'cenas' object has no attribute 'cena1'
 '''},
+{'date': 'Mon Sep 20 2021 17:23:37.972 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 281
+    jogo.cena1()
+AttributeError: 'cenas' object has no attribute 'cena1'
+'''},
