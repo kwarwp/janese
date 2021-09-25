21af3b2e768a39f3596f8351bb7ca7b520a7f0d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-25 00:06:15 -0300

    Saved adda/__log__.py @2021-09-25 03:06

--- adda/__log__.py
+++ adda/__log__.py
@@ -377,3 +377,21 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Sep 25 2021 00:06:05.45 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 364
+    jogo = cenas()
+  module <module> line 67
+    self.cenas = [self.c11, self.c12, self.c21, self.c22, self.c31, self.c32, self.c33]
+AttributeError: 'cenas' object has no attribute 'c33'
+'''},
